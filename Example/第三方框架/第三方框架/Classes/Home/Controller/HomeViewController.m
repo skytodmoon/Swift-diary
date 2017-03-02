41dba4949603a4035507316d7d4ef3fb4f86bd88
金亮齐 <15914115336@163.com>
2017-03-02 23:52:57 +0800

    提交源码
    提交源码

--- Example/第三方框架/第三方框架/Classes/Home/Controller/HomeViewController.m
+++ Example/第三方框架/第三方框架/Classes/Home/Controller/HomeViewController.m
@@ -7,31 +7,116 @@
 //
 
 #import "HomeViewController.h"
+#import "MJRefresh.h"
 
 @interface HomeViewController ()
-
+@property(nonatomic, strong) UITableView *tableView;
 @end
 
 @implementation HomeViewController
 
 - (void)viewDidLoad {
+    
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
     [super viewDidLoad];
+    
+    [self setNav];
+    
+    [self initTableView];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)setNav{
+    UIView *backView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screen_width, 98)];
+    backView.backgroundColor = navigationBarColor;
+    [self.view addSubview:backView];
+    
+    //声明：原创所有，不要注释下面的UIButton
+    UIButton *nameBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    nameBtn.frame = CGRectMake(10, 20, 60, 40);
+    nameBtn.titleLabel.font = [UIFont systemFontOfSize:15];
+    [nameBtn setTitle:@"点击这" forState:UIControlStateNormal];
+    [nameBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [nameBtn addTarget:self action:@selector(OnNameBtn) forControlEvents:UIControlEventTouchUpInside];
+    [backView addSubview:nameBtn];
+    
+    
+    //标题
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(screen_width/2-80, 20, 160, 30)];
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.text = @"百度传课";
+    titleLabel.font = [UIFont systemFontOfSize:19];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    [backView addSubview:titleLabel];
+    //搜索
+    UIButton *searchBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    searchBtn.frame = CGRectMake(screen_width-10-40, 20, 40, 40);
+    [searchBtn setImage:[UIImage imageNamed:@"search_btn_unpre_bg"] forState:UIControlStateNormal];
+    [searchBtn addTarget:self action:@selector(OnSearchBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [backView addSubview:searchBtn];
+    
+    //
+    NSArray *segmentArray = [[NSArray alloc] initWithObjects:@"精选推荐",@"课程分类", nil];
+    UISegmentedControl *segmentCtr = [[UISegmentedControl alloc] initWithItems:segmentArray];
+    segmentCtr.frame = CGRectMake(36, 64, screen_width-36*2, 30);
+    segmentCtr.selectedSegmentIndex = 0;
+    
+    NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:[UIFont boldSystemFontOfSize:15],NSFontAttributeName,[UIColor whiteColor], NSForegroundColorAttributeName, nil];
+    [segmentCtr setTitleTextAttributes:attributes forState:UIControlStateNormal];
+    NSDictionary *highlightedAttributes = [NSDictionary dictionaryWithObject:[UIColor whiteColor] forKey:NSForegroundColorAttributeName];
+    [segmentCtr setTitleTextAttributes:highlightedAttributes forState:UIControlStateHighlighted];
+    
+    segmentCtr.tintColor = RGB(46, 158, 138);
+    [segmentCtr addTarget:self action:@selector(OnTapSegmentCtr:) forControlEvents:UIControlEventValueChanged];
+    [backView addSubview:segmentCtr];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)OnNameBtn{
+    UIAlertView *alertVC = [[UIAlertView alloc] initWithTitle:@"关于作者" message:@"作者：QJL，QQ：274721774，例子说明，转载请注明出处，不可用于商业用途及其他不合法用途。" delegate:self cancelButtonTitle:@"同意" otherButtonTitles:nil, nil];
+    [alertVC show];
 }
-*/
+
+
+-(void)initTableView{
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 98, screen_width, screen_height-98-49) style:UITableViewStylePlain];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self setupTableview];
+}
+
+
+-(void)setupTableview{
+    //添加下拉的动画图片
+    //设置下拉刷新回调
+    [self.tableView addGifHeaderWithRefreshingTarget:self refreshingAction:@selector(loadNewData)];
+    
+    //设置普通状态的动画图片
+    NSMutableArray *idleImages = [NSMutableArray array];
+    for (NSUInteger i = 1; i<=60; ++i) {
+        //        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"dropdown_anim__000%zd",i]];
+        //        [idleImages addObject:image];
+        UIImage *image = [UIImage imageNamed:@"icon_listheader_animation_1"];
+        [idleImages addObject:image];
+    }
+    [self.tableView.gifHeader setImages:idleImages forState:MJRefreshHeaderStateIdle];
+    
+    //设置即将刷新状态的动画图片
+    NSMutableArray *refreshingImages = [NSMutableArray array];
+    UIImage *image1 = [UIImage imageNamed:@"icon_listheader_animation_1"];
+    [refreshingImages addObject:image1];
+    UIImage *image2 = [UIImage imageNamed:@"icon_listheader_animation_2"];
+    [refreshingImages addObject:image2];
+    [self.tableView.gifHeader setImages:refreshingImages forState:MJRefreshHeaderStatePulling];
+    
+    //设置正在刷新是的动画图片
+    [self.tableView.gifHeader setImages:refreshingImages forState:MJRefreshHeaderStateRefreshing];
+    
+    //马上进入刷新状态
+    [self.tableView.gifHeader beginRefreshing];
+}
 
 @end
