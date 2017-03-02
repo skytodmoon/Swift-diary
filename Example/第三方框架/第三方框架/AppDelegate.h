b36171ec947fb9483c97f0d6a9aba7af13592e7a
金亮齐 <15914115336@163.com>
2017-03-02 22:59:21 +0800

    提交oc第三方例子
    提交oc第三方例子

---
+++ Example/第三方框架/第三方框架/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  第三方框架
+//
+//  Created by 金亮齐 on 2017/3/2.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
