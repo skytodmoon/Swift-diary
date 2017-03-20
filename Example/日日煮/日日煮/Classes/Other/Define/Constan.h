f82c6b290d987ee873a42de4490ecd89557e3f61
金亮齐 <15914115336@163.com>
2017-03-20 17:39:17 +0800

    提交项目文件管理划分
    提交项目文件管理划分

---
+++ Example/日日煮/日日煮/Classes/Other/Define/Constan.h
@@ -0,0 +1,36 @@
+//
+//  Constan.h
+//  日日煮
+//
+//  Created by 金亮齐 on 2017/3/20.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#ifndef Constan_h
+#define Constan_h
+
+/** 屏幕宽度 */
+#define kScreenW [UIScreen mainScreen].bounds.size.width
+
+/** 屏幕高度 */
+#define kScreenH [UIScreen mainScreen].bounds.size.height
+
+/** 三原色的设置 */
+#define kRGBColor(R,G,B,A) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:A]
+
+/** Documents文件夹的路径 */
+#define kDocPath NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject
+
+/** appdelegate的实例对象 */
+#define kAppDelegate ((AppDelegate *)[UIApplication sharedApplication].delegate)
+
+/** 把self转化为 __weak __block的方式, 方便的在block中使用而不导致内存循环应用问题 */
+
+/** 在宏中使用 \ 可以换行 */
+#define WK(weakSelf) \
+__weak __typeof(&*self)weakSelf = self;\
+
+/** Bundle路径 */
+#define kMyBundlePath [[NSBundle mainBundle]pathForResource:@"MyBundle" ofType:@"bundle"]
+
+#endif /* Constan_h */
---
+++ Example/日日煮/日日煮/Classes/Other/Define/RequestPaths.h
@@ -0,0 +1,25 @@
+//
+//  RequestPaths.h
+//  日日煮
+//
+//  Created by 金亮齐 on 2017/3/20.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#ifndef RequestPaths_h
+#define RequestPaths_h
+
+#define kBasePath @"http://api.daydaycook.com.cn"
+
+#define kDayDayCookPath @"/daydaycook/server/recipe/search.do"
+
+#define kCookMenu @"http://api.daydaycook.com.cn/daydaycook/h5/recipe/loadContent.do?id=%ld&languageId=&mainland="
+
+#define kMobAppKey @"1418ca8f4b8ce"
+
+#define kMobAppSecret @"c2ae895eb6899874af5e710da15c2dd0"
+
+#define kUMengKey @"577f37f1e0f55abda40037c3"
+
+
+#endif /* RequestPaths_h */
