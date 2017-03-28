aeaf5f1d0375feb41299a87c5c72c8759804b897
醉看红尘这场梦 <15914115336@163.com>
2017-03-28 11:06:15 +0800

    添加下拉刷新
    添加下拉刷新

---
+++ Example/小日子/小日子/Classes/Tools(工具)/SDRefreshHeader.swift
@@ -0,0 +1,36 @@
+//
+//  SDRefreshHeader.swift
+//  小日子
+//
+//  Created by 金亮齐 on 2017/3/28.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+class SDRefreshHeader: MJRefreshGifHeader {
+
+    override func prepare() {
+        super.prepare()
+        stateLabel!.hidden = true
+        lastUpdatedTimeLabel!.hidden = true
+        
+        let idleImages = NSMutableArray()
+        let idImage = UIImage(named: "wnx00")
+        idleImages.addObject(idImage!)
+        setImages(idleImages as [AnyObject], forState: MJRefreshStateIdle)
+        
+        let refreshingImages = NSMutableArray()
+        let refreshingImage = UIImage(named: "wnx00")!
+        refreshingImages.addObject(refreshingImage)
+        setImages(refreshingImages as [AnyObject], forState: MJRefreshStatePulling)
+        
+        let refreshingStartImages = NSMutableArray()
+        for i in 0...92 {
+            let image = UIImage(named: String(format: "wnx%02d", i))
+            refreshingStartImages.addObject(image!)
+        }
+        setImages(refreshingStartImages as [AnyObject], forState: MJRefreshStateRefreshing)
+        
+    }
+}
