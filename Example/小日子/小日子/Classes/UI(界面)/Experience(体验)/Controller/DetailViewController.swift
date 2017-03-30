1e972c8067a85843ef0c31fcf8d92f8c087dd851
醉看红尘这场梦 <15914115336@163.com>
2017-03-30 18:59:49 +0800

    提交自定义视图
    提交自定义视图

--- Example/小日子/小日子/Classes/UI(界面)/Experience(体验)/Controller/DetailViewController.swift
+++ Example/小日子/小日子/Classes/UI(界面)/Experience(体验)/Controller/DetailViewController.swift
@@ -10,7 +10,44 @@ import UIKit
 /// 详情ViewController顶部图片的高度
 public let DetailViewController_TopImageView_Height: CGFloat = 225
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController,UIActionSheetDelegate {
+    
+    // 优化性能,防止重复设置
+    private var showBlackImage: Bool = false
+    private var htmlNewString: NSMutableString?
+    private let scrollShowNavH: CGFloat = DetailViewController_TopImageView_Height - NavigationH
+    private let imageW: CGFloat = AppWidth - 23.0
+    private var signUpBtn: UIButton!
+    private var isLoadFinish = false
+    private var isAddBottomView = false
+    private var loadFinishScrollHeihgt: CGFloat = 0
+    private lazy var bottomViews: [ExploreBottomView] = [ExploreBottomView]()
+    private lazy var shareView: ShareView = ShareView.shareViewFromXib()
+    private lazy var backBtn: UIButton = UIButton()
+    private lazy var likeBtn: UIButton = UIButton()
+    private lazy var sharedBtn: UIButton = UIButton()
+    private lazy var webView: UIWebView = ExperienceWebView(frame: MainBounds, webViewDelegate: self, webViewScrollViewDelegate: self)
+    
+    
+    private lazy var topImageView: UIImageView = {
+        let image = UIImageView(frame: CGRectMake(0, 0, AppWidth, DetailViewController_TopImageView_Height))
+        image.image = UIImage(named: "quesheng")
+        image.contentMode = UIViewContentMode.ScaleToFill
+        return image
+    }()
+    
+    private lazy var phoneActionSheet: UIActionSheet? = {
+        let action = UIActionSheet(title: nil, delegate: self, cancelButtonTitle: "取消", destructiveButtonTitle: self.model!.telephone!)
+        return action
+    }()
+    
+    private lazy var customNav: UIView = {
+        let customNav = UIView(frame: CGRectMake(0, 0, AppWidth, NavigationH))
+        customNav.backgroundColor = UIColor.whiteColor()
+        customNav.alpha = 0.0
+        return customNav
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
