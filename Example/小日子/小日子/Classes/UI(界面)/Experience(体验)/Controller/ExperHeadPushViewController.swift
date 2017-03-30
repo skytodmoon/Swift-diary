e511638dab07aed01963a2f5b7a26ae67b7ce8ec
醉看红尘这场梦 <15914115336@163.com>
2017-03-30 18:56:22 +0800

    提交ExperHeadPushViewController
    提交ExperHeadPushViewController

--- Example/小日子/小日子/Classes/UI(界面)/Experience(体验)/Controller/ExperHeadPushViewController.swift
+++ Example/小日子/小日子/Classes/UI(界面)/Experience(体验)/Controller/ExperHeadPushViewController.swift
@@ -9,27 +9,55 @@
 import UIKit
 
 class ExperHeadPushViewController: UIViewController {
+    
+    var model:ExperienceHeadModel? {
+        didSet {
+            webView?.loadRequest(NSURLRequest(URL: NSURL(string: model!.mobileURL!)!))
+            navigationItem.title = model!.title
+            shareView.shareModel = ShareModel(shareTitle: model!.title, shareURL: model!.shareURL, image: nil, shareDetail: model!.title)
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = theme.SDBackgroundColor
+        view.addSubview(webView!)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(imageName: "share_1", highlImageName: "share_2", targer: self, action: #selector(ExperHeadPushViewController.sharedClick))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    /// MARK:- 懒加载对象
+    private lazy var webView: UIWebView? = {
+        let webView = UIWebView(frame: MainBounds)
+        webView.delegate = self
+        webView.backgroundColor = theme.SDBackgroundColor
+        webView.hidden = true
+        return webView
+    }()
+    
+    private lazy var shareView: ShareView = {
+        let shareView = ShareView.shareViewFromXib()
+        return shareView
+    }()
+    
+    private lazy var loadImage = LoadAnimatImageView.sharedManager
+    
+    func sharedClick() {
+        view.addSubview(shareView)
+        shareView.shareVC = self
+        shareView.showShareView(CGRectMake(0, AppHeight - 215 - NavigationH, AppWidth, 215))
+    }
+}
+
+extension ExperHeadPushViewController: UIWebViewDelegate {
+    func webViewDidStartLoad(webView: UIWebView) {
+        loadImage.startLoadAnimatImageViewInView(view, center: view.center)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func webViewDidFinishLoad(webView: UIWebView) {
+        webView.hidden = false
+        loadImage.stopLoadAnimatImageView()
+        webView.scrollView.contentSize.height += NavigationH
     }
-    */
-
 }
