1c1d062b9b1820f1900c52ecec564da16e3c1fa9
醉看红尘这场梦 <15914115336@163.com>
2017-03-31 09:39:52 +0800

    添加自定义view，button，textfield
    添加自定义view，button，textfield

---
+++ Example/小日子/小日子/Classes/UI(界面)/Classify(分类)/View/SearchTextField.swift
@@ -0,0 +1,21 @@
+//
+//  SearchTextField.swift
+//  小日子
+//
+//  Created by 金亮齐 on 2017/3/31.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+class SearchTextField: UITextField {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
