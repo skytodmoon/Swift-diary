9891a6a7b20e101f34ab99b867758a9b3155a14b
醉看红尘这场梦 <15914115336@163.com>
2017-03-31 16:11:58 +0800

    提交图片相详情
    提交图片相详情

--- Example/爆侃网文/爆侃网文/Classes/UI(界面)/Photo/Model/PhotoDetailModel.swift
+++ Example/爆侃网文/爆侃网文/Classes/UI(界面)/Photo/Model/PhotoDetailModel.swift
@@ -9,5 +9,26 @@
 import UIKit
 
 class PhotoDetailModel: NSObject {
+    
+    /// 图片标题
+    var title: String?
+    
+    /// 图片描述
+    var text: String?
+    
+    /// 图片url
+    var picurl: String?
+    
+    /**
+     字典转模型构造方法
+     */
+    init(dict: [String : String]) {
+        super.init()
+        setValuesForKeysWithDictionary(dict)
+    }
+    
+    override func setValue(value: AnyObject?, forUndefinedKey key: String) {}
+    
+
 
 }
