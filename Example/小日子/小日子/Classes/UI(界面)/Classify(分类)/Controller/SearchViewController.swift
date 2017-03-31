b80d6d6e7a82fb747768e6f9bfeda5a2f07b2062
醉看红尘这场梦 <15914115336@163.com>
2017-03-31 09:37:33 +0800

    提交发现分类
    提交发现分类

--- Example/小日子/小日子/Classes/UI(界面)/Classify(分类)/Controller/SearchViewController.swift
+++ Example/小日子/小日子/Classes/UI(界面)/Classify(分类)/Controller/SearchViewController.swift
@@ -1,27 +1,14 @@
 //
-//  ClassDetailViewController.swift
+//  SearchViewController.swift
 //  小日子
 //
-//  Created by 金亮齐 on 2017/3/27.
+//  Created by 金亮齐 on 2017/3/31.
 //  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
 //
 
 import UIKit
 
-public let DetailCellHeight: CGFloat = 220
-
-class ClassDetailViewController: UIViewController {
-    
-    
-    private lazy var detailTableView: MainTableView = {
-        let tableView = MainTableView(frame: MainBounds, style: .Plain, dataSource: self, delegate: self)
-        tableView.rowHeight = DetailCellHeight
-        tableView.contentInset = UIEdgeInsetsMake(0, 0, NavigationH, 0)
-        tableView.registerNib(UINib(nibName: "DetailCell", bundle: nil), forCellReuseIdentifier: SD_DetailCell_Identifier)
-        return tableView
-    }()
-    
-    var details: DetailModel?
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
