4bc597dbb655b4b6fbef99d26f967a9b41016b22
醉看红尘这场梦 <15914115336@163.com>
2017-04-10 16:49:55 +0800

    提交appdelegate
    提交appdelegate方法

--- Example/爱鲜蜂/爱鲜蜂/AppDelegate.swift
+++ Example/爱鲜蜂/爱鲜蜂/AppDelegate.swift
@@ -15,32 +15,91 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+//        setUM()
+        
+        setAppSubject()
+        
+        addNotification()
+        
+        buildKeyWindow()
+        
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func addNotification() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "showMainTabbarControllerSucess:", name: ADImageLoadSecussed, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "showMainTabbarControllerFale", name: ADImageLoadFail, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "shoMainTabBarController", name: GuideViewControllerDidFinish, object: nil)
     }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
+    // MARK: - Action
+    func showMainTabbarControllerSucess(noti: NSNotification) {
+        let adImage = noti.object as! UIImage
+        let mainTabBar = MainTabBarController()
+        mainTabBar.adImage = adImage
+        window?.rootViewController = mainTabBar
     }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    // MARK: - Action
+    func showMainTabbarControllerSucess(noti: NSNotification) {
+        let adImage = noti.object as! UIImage
+        let mainTabBar = MainTabBarController()
+        mainTabBar.adImage = adImage
+        window?.rootViewController = mainTabBar
     }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    func showMainTabbarControllerFale() {
+        window!.rootViewController = MainTabBarController()
     }
-
+    
+    func shoMainTabBarController() {
+        window!.rootViewController = MainTabBarController()
+    }
+    
+    
+    // MARK:- privete Method
+    // MARK:主题设置
+    private func setAppSubject() {
+        let tabBarAppearance = UITabBar.appearance()
+        tabBarAppearance.backgroundColor = UIColor.whiteColor()
+        tabBarAppearance.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+        
+        let navBarnAppearance = UINavigationBar.appearance()
+        navBarnAppearance.translucent = false
+    }
+    
+    // MARK: - Public Method
+    private func buildKeyWindow() {
+        
+        window = UIWindow(frame: ScreenBounds)
+        window!.makeKeyAndVisible()
+        
+        let isFristOpen = NSUserDefaults.standardUserDefaults().objectForKey("isFristOpenApp")
+        
+        if isFristOpen == nil {
+            window?.rootViewController = GuideViewController()
+            NSUserDefaults.standardUserDefaults().setObject("isFristOpenApp", forKey: "isFristOpenApp")
+        } else {
+            loadADRootViewController()
+        }
+    }
+    
+    func loadADRootViewController() {
+        adViewController = ADViewController()
+        
+        weak var tmpSelf = self
+        MainAD.loadADData { (data, error) -> Void in
+            if data?.data?.img_name != nil {
+                tmpSelf!.adViewController!.imageName = data!.data!.img_name
+                tmpSelf!.window?.rootViewController = self.adViewController
+            }
+        }
+    }
 
 }
 
