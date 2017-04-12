c35a5483fde3feb8bdc10fadb8899d88dc9bffc1
醉看红尘这场梦 <15914115336@163.com>
2017-04-12 13:03:03 +0800

    请求数据成功
    请求数据成功

--- Example/堆糖/堆糖/Classes/Explorer(发现)/Controller/ExploreMainController.swift
+++ Example/堆糖/堆糖/Classes/Explorer(发现)/Controller/ExploreMainController.swift
@@ -9,55 +9,74 @@
 import UIKit
 import Alamofire
 
+
+private let ExploreMainCellID = "ExploreMainCell"
 private let urlString = "http://www.duitang.com/napi/index/groups/?app_code=gandalf&app_version=5.9%20rv%3A150681&device_name=Unknown%20iPhone&device_platform=iPhone6%2C1&locale=zh_CN&platform_name=iPhone%20OS&platform_version=9.2.1&screen_height=568&screen_width=320"
 
 class ExploreMainController: UIViewController {
+    
+    private var tableView: UITableView!
+    
+    var models = [ExploreMainModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        sendRequestRequest()
+        
+        setupTableview()
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setupTableview() {
+        tableView = UITableView(frame: view.bounds, style: .Plain)
+        tableView.backgroundColor = UIColor.colorWith(245, green: 245, blue: 245, alpha: 1)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerNib(UINib(nibName: ExploreMainCellID, bundle: nil), forCellReuseIdentifier: ExploreMainCellID)
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        tableView.contentInset.top = 5
+        view.addSubview(tableView)
+        sendRequestRequest()
     }
     
     
     private func sendRequestRequest() {
 
-        // Add Headers
-        let headers = [
-            "Cookie":"sessionid=cb7df479-c133-4a21-ae40-993de9faf434",
-            ]
-        
-        // Add URL parameters
-        let urlParams = [
-            "app_code":"gandalf",
-            "app_version":"5.9 rv:150681",
-            "device_name":"Unknown iPhone",
-            "device_platform":"iPhone6,1",
-            "locale":"zh_CN",
-            "platform_name":"iPhone OS",
-            "platform_version":"9.2.1",
-            "screen_height":"568",
-            "screen_width":"320",
-            ]
-        
-        // Fetch Request
-        Alamofire.request(.GET, "http://www.duitang.com/napi/index/groups/", parameters: urlParams, headers: headers)
-            .validate(statusCode: 200..<300)
-            .responseJSON { response in
-                if (response.result.error == nil) {
-                    print("请求成功")
-                }
-                else {
-                    print("请求失败")
-                }
+        NetWorkTool.sharedInstance.get(urlString, parameters: nil, success: { (response) -> () in
+            
+            self.models = ExploreMainModel.loadExploreMainModels(response!)
+            
+            
+            self.tableView.reloadData()
+            
+        }) { (error) -> () in
+            
+            DEBUGLOG(error)
+            
         }
     }
-
-
 }
+
+extension ExploreMainController: UITableViewDataSource,UITableViewDelegate {
+     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(ExploreMainCellID) as! ExploreMainCell
+        cell.exploreMainModel = models[indexPath.row]
+        cell.delegate = self
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return indexPath.row == 0 ? 180 : 120
+    }
+}
+extension ExploreMainController: ExploreMainCellDetegate {
+    func exploreCollectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+}
+
--- Example/堆糖/堆糖/Classes/Other(其他)/Extension/UIColor+Extension.swift
+++ Example/堆糖/堆糖/Classes/Other(其他)/Extension/UIColor+Extension.swift
@@ -24,4 +24,10 @@ extension UIColor{
         return UIColor(red: 223 / 255.0, green: 52 / 255.0, blue: 46 / 255.0 , alpha: 1.0)
     }
     
+    /**设置统一颜色的扩展*/
+    class func colorWith(red: Int, green: Int, blue: Int, alpha: CGFloat) -> UIColor {
+        let color = UIColor(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha)
+        return color
+    }
+    
 }
