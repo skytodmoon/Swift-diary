5f39bb23ad1b029f02b14ef78a3527ccaf3fc430
醉看红尘这场梦 <15914115336@163.com>
2017-04-13 11:13:03 +0800

    添加热门控制器
    添加热门控制器

---
+++ Example/堆糖/堆糖/Classes/Club(Club)/ClubHot(热门)/View/ClubHotCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClubHotCell.swift
+//  堆糖
+//
+//  Created by 金亮齐 on 2017/4/13.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+class ClubHotCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Example/堆糖/堆糖/Classes/Store(商店)/Controller/StoreController.swift
+++ Example/堆糖/堆糖/Classes/Store(商店)/Controller/StoreController.swift
@@ -8,28 +8,52 @@
 
 import UIKit
 
+private let urlString = "http://buy.duitang.com/buy/#/entry?from=app&_urlopentype=page&app_version=5.9&app_code=gandalf&__urlopentype=pageweb&_k=h6lmof"
+
 class StoreController: UIViewController {
+    
+    private var button : UIButton!
+    private var webView = UIWebView()
+    override func loadView() {
+        view = webView
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let request =  NSURLRequest(URL: NSURL(string: urlString)!)
+        webView.loadRequest(request)
+        button = UIButton(type: UIButtonType.Custom)
+        button.setBackgroundImage(UIImage(named: "icon_back_dark"), forState: UIControlState.Normal)
+        button.addTarget(self, action: #selector(StoreController.leftBarButtonClick), forControlEvents: UIControlEvents.TouchUpInside)
+        button.sizeToFit()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: button)
+        webView.delegate = self
+
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc private func leftBarButtonClick(){
+        
+        webView.goBack()
+    }
+}
+
+extension StoreController: UIWebViewDelegate {
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        button.hidden = !webView.canGoBack
+        navigationItem.title = "加载中.."
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
+        
     }
-    */
-
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        navigationItem.title = "堆糖商店"
+        
+    }
 }
