37d10a3615189690a4a3e3885e9709f5f248327d
醉看红尘这场梦 <15914115336@163.com>
2017-04-13 18:01:28 +0800

    提交第一，第二章
    提交第一，第二章

---
+++ Swift(2.0)/1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+import XCPlayground
+
+let image = UIImage(named: "icon.png")
+
+let demoView = UIView(frame: CGRectMake(0, 0, 250, 250))
+demoView.backgroundColor = UIColor.redColor()
+
+XCPlaygroundPage.currentPage.liveView = demoView
+
+var i = 1
+
+for x in 1..<5 {
+    i = i * x
+    
+    XCPlaygroundPage.currentPage.captureValue(i, withIdentifier: "i")
+}
+print(i)
+print("Hello", terminator: " ")
+print("World", terminator: ",")
+print("playground")
+print(20, 18, 39, "Hello", "playground", separator: "|")
