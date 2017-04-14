203ff9ba38dc944e44ea0a8f08ef0b9586d69114
醉看红尘这场梦 <15914115336@163.com>
2017-04-14 08:42:46 +0800

    添加数组
    添加数组

--- Swift(2.0)/7/数组.playground/Contents.swift
+++ Swift(2.0)/7/数组.playground/Contents.swift
@@ -2,4 +2,42 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var studentList1: Array<String> = ["张三","李四","王五","董六"]
+
+var studentList2: [String]  = ["张三","李四","王五","董六"]
+
+let studentList3: [String]  = ["张三","李四","王五","董六"]
+
+var studentList4 = [String]()
+
+
+//可变数组
+var studentList: [String]  = ["张三","李四","王五"]
+print(studentList)
+
+studentList.append("董六")
+print(studentList)
+
+studentList += ["刘备", "关羽"]
+print(studentList)
+
+studentList.insert("张飞",atIndex:studentList.count)
+print(studentList)
+
+let removeStudent = studentList.removeAtIndex(0)
+print(studentList)
+
+studentList[0] = "诸葛亮"
+print(studentList)
+
+//数组遍历
+var studentList9: [String]  = ["张三","李四","王五"]
+
+for item in studentList9 {
+    print(item)
+}
+
+for (index, value) in studentList9.enumerate() {
+    print("Item \(index + 1 ) : \(value)")
+}
+
