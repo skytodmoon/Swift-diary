a77db06b77164c60544318ed7db9414693e03108
醉看红尘这场梦 <15914115336@163.com>
2017-04-17 19:28:29 +0800

    修复bug
    修复bug

--- 王巍Swift100点/Swift-100/Classes/ASSOCIATED OBJECT.playground/Contents.swift
+++ 王巍Swift100点/Swift-100/Classes/ASSOCIATED OBJECT.playground/Contents.swift
@@ -7,7 +7,7 @@ import UIKit
 //得益于 Objective-C 的运行时和 Key-Value Coding 的特性，我们可以在运行时向一个对象添加值存储。而在使用 Category 扩展现有的类的功能的时候，直接添加实例变量这种行为是不被允许的，这时候一般就使用 property 配合 Associated Object 的方式，将一个对象 “关联” 到已有的要扩展的对象上。进行关联后，在对这个目标对象访问的时候，从外界看来，就似乎是直接在通过属性访问对象的实例变量一样，可以非常方便。
 
 //在 Swift 中这样的方法依旧有效，只不过在写法上可能有些不同。两个对应的运行时的 get 和 set Associated Object 的 API 是这样的：
-
+/*
 func objc_getAssociatedObject(object: AnyObject!,
                               key: UnsafePointer<Void>
     )  -> AnyObject!
@@ -17,7 +17,7 @@ func objc_setAssociatedObject(object: AnyObject!,
                               value: AnyObject!,
                               policy: objc_AssociationPolicy)
 这两个 API 所接受的参数也都 Swift 化了，并且因为 Swift 的安全性，在类型检查上严格了不少，因此我们有必要也进行一些调整。在 Swift 中向某个 extension 里使用 Associated Object 的方式将对象进行关联的写法是：
-
+*/
 // MyClass.swift
 class MyClass {
 }
@@ -58,5 +58,7 @@ printTitle(a)
 // 没有设置
 // Title: Swifter.tips
 //key 的类型在这里声明为了 Void?，并且通过 & 操作符取地址并作为 UnsafePointer<Void> 类型被传入。这在 Swift 与 C 协作和指针操作时是一种很常见的用法。关于 C 的指针操作和这些 unsafe 开头的类型的用法，可以参看 UnsafePointer 一节的内容。
+
+
 
 
