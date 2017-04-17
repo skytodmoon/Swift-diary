4f6d53fd1c8fc2f6d153223f69cfc8b648262ba5
金亮齐 <15914115336@163.com>
2017-04-17 22:10:46 +0800

    Swift3.0第一章
    Swift3.0第一章

---
+++ Swift3.0/Swift3.0PlayGround/Swift3.0/Swift3.0/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift3.0
+//
+//  Created by 金亮齐 on 2017/4/17.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Swift3.0/Swift3.0PlayGround/Swift3.0/第一章/MyPlayground2.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+
+
+import Foundation
+
+var str = "Hello World"
+print(str)
+
+print("Hello", terminator: " ")
+print("World", terminator: ",")
+print("playground")
+
+print(20, 18, 39, "Hello", "playground", separator: "|")
