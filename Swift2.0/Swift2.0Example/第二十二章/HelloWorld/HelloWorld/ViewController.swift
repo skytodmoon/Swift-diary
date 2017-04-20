5bd68e087d295a087d7ff8654588e5077cff2a2b
醉看红尘这场梦 <15914115336@163.com>
2017-04-20 11:56:34 +0800

    提交Game
    提交Game

---
+++ Swift2.0/Swift2.0Example/第二十二章/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 金亮齐 on 2017/4/20.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
