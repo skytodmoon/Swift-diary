390156424f973afb7f3f6cd75006787988f3fe77
金亮齐 <15914115336@163.com>
2017-04-24 21:40:45 +0800

    提交第七章
    提交第七章

---
+++ Swift3.0/Swift3.0PlayGround/Swift3.0/第七章/数组声明与初始化.playground/section-1.swift
@@ -0,0 +1,9 @@
+
+
+var studentList1: Array<String> = ["张三","李四","王五","董六"]
+
+var studentList2: [String]  = ["张三","李四","王五","董六"]
+
+let studentList3: [String]  = ["张三","李四","王五","董六"]
+
+var studentList4 = [String]()
