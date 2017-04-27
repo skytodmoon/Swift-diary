1faca039678b1580eb0c81783b3cca06c9b6d340
醉看红尘这场梦 <15914115336@163.com>
2017-04-27 19:03:09 +0800

    实现对象模板模式
    实现对象模板模式

--- Swift设计模式/第一章/Tuples.playground/section-1.swift
+++ Swift设计模式/第一章/Tuples.playground/section-1.swift
@@ -11,3 +11,60 @@ func writeProductDetails(product: (String, String, String, Double, Int)) {
 }
 
 writeProductDetails(myProduct);
+
+
+
+/*实现对象模板模式*/
+
+class Product {
+    
+    var name:String;
+    var price:Double;
+    private var stockBackingValue:Int = 0;
+    
+    var stock:Int {
+        get {
+            return stockBackingValue;
+        }
+        set {
+            stockBackingValue = max(0, newValue);
+        }
+    }
+    
+    init(name:String, price:Double, stock:Int) {
+        self.name = name;
+        self.price = price;
+        self.stock = stock;
+    }
+    
+    func calculateTax(rate: Double) -> Double {
+        return min(10, self.price * rate);
+    }
+    
+    var stockValue: Double {
+        get {
+            return self.price * Double(self.stock);
+        }
+    }
+}
+
+var products = [
+    Product(name: "Kayak", price: 275, stock: 10),
+    Product(name: "Lifejacket", price: 48.95, stock: 14),
+    Product(name: "Soccer Ball", price: 19.5, stock: 32)];
+
+func calculateStockValue(productsArray:[Product]) -> Double {
+    return productsArray.reduce(0, combine: {(total, product) -> Double in
+        return total + product.stockValue;
+    });
+}
+
+print("Sales tax for Kayak: $\(products[0].calculateTax(0.2))");
+print("Total value of stock: $\(calculateStockValue(products))");
+products[0].stock = -50;
+print("Stock Level for Kayak: \(products[0].stock)");
+
+
+
+
+
