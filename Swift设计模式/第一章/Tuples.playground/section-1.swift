811eea2321a8b992ccb5c6aa0753500e5086a328
醉看红尘这场梦 <15914115336@163.com>
2017-04-27 11:51:51 +0800

    提交Swift设计模式
    提交Swift设计模式

---
+++ Swift设计模式/第一章/Tuples.playground/section-1.swift
@@ -0,0 +1,13 @@
+import Foundation;
+
+var myProduct = ("Kayak", "A boat for one person", "Watersports", 275.0, 10);
+
+func writeProductDetails(product: (String, String, String, Double, Int)) {
+    print("Name: \(product.0)");
+    print("Description: \(product.1)");
+    print("Category: \(product.2)");
+    let formattedPrice = NSString(format: "$%.2lf", product.3);
+    print("Price: \(formattedPrice)");
+}
+
+writeProductDetails(myProduct);
