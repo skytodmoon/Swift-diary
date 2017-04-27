97bd9c08c9674390d30a649d4028aef35f8828a3
金亮齐 <15914115336@163.com>
2017-04-27 23:35:00 +0800

    提交第八章

---
+++ Swift3.0/Swift3.0PlayGround/Swift3.0/第八章/可变参数.playground/section-1.swift
@@ -0,0 +1,13 @@
+
+
+func sum(numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total
+}
+
+sum(numbers: 100.0, 20, 30)
+
+sum(numbers: 30, 80)
