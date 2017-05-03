6fda17ee87200534fc5d1b4ff42e94d61743777a
醉看红尘这场梦 <15914115336@163.com>
2017-05-03 19:45:19 +0800

    UILabel+Extension
    UILabel+Extension

---
+++ Example/微博/微博/Classes/Other/Tools/UILabel+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UILabel+Extension.swift
+//  微博
+//
+//  Created by 金亮齐 on 2017/5/3.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    //MARK: - 快速创建UILabel
+    class func createLabel(color: UIColor, fontSize: CGFloat) -> UILabel {
+        let label = UILabel()
+        label.textColor = color
+        label.font = UIFont.systemFontOfSize(fontSize)
+        return label
+    }
+}
