aa9f57f346003c8e8ac2635165f820cc47fdac45
醉看红尘这场梦 <15914115336@163.com>
2017-05-05 14:09:16 +0800

    设置首页的导航栏

--- Example/Swift项目/微博/微博/Classes/Home/Controller/HomeViewController.swift
+++ Example/Swift项目/微博/微博/Classes/Home/Controller/HomeViewController.swift
@@ -12,7 +12,8 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupNav()
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +22,8 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: - 设置导航的左右按钮
+    private func setupNav() {
+        
     }
-    */
-
 }
---
+++ Example/Swift项目/微博/微博/Classes/Main/View/VisitorView.swift
@@ -0,0 +1,21 @@
+//
+//  VisitorView.swift
+//  微博
+//
+//  Created by 金亮齐 on 2017/5/5.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+class VisitorView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
