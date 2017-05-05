15a7184fdb1c3498034f4d2df8b5c27396b88139
金亮齐 <15914115336@163.com>
2017-05-06 00:46:33 +0800

    提交第二部分

---
+++ Swift3.0/Swift3.0PlayGround/Swift3.0/第十章/第二部分/open和public示例.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Foundation
+//import ModuleA
+
+/////模块B
+//class ClassB : ClassA {
+//    public override func printName() {
+//        print(name)
+//    }
+//}
+//
+//print("Hello, World!")
+//
+/////模块A
+//open class ClassA {
+//    public var name: String = "Tony"
+//    open func printName() {
+//        print(name)
+//    }
+//}
+
+
+//#import <Cocoa/Cocoa.h>
+//
+////! Project version number for ModuleA.
+//FOUNDATION_EXPORT double ModuleAVersionNumber;
+//
+////! Project version string for ModuleA.
+//FOUNDATION_EXPORT const unsigned char ModuleAVersionString[];
+//
+//// In this header, you should import all the public headers of your framework using statements like #import <ModuleA/PublicHeader.h>
