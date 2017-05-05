ac2ee2c90df7e344730913334d6d56d68b273fa7
醉看红尘这场梦 <15914115336@163.com>
2017-05-05 14:48:19 +0800

    ’完成首页导航栏设置‘

--- Example/Swift项目/微博/微博/Classes/Home/Controller/HomeViewController.swift
+++ Example/Swift项目/微博/微博/Classes/Home/Controller/HomeViewController.swift
@@ -22,15 +22,23 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    //MARK: - 设置导航的左右按钮
+    //MARK: - 设置导航的左右按钮和中间的按钮
     private func setupNav() {
         navigationItem.leftBarButtonItem = UIBarButtonItem.createBarButtonItem("navigationbar_friendattention", target: self, action: #selector(HomeViewController.leftItemClick))
         navigationItem.rightBarButtonItem = UIBarButtonItem.createBarButtonItem("navigationbar_pop", target: self, action: #selector(HomeViewController.rightItemClick))
+        //点击中间按钮
+        let titleBtn = TitleButton()
+        titleBtn.setTitle("醉看红尘这场梦", forState: .Normal)
+        titleBtn .addTarget(self, action: #selector(HomeViewController.titltBtnClick(_:)), forControlEvents: .TouchUpInside)
+        navigationItem.titleView = titleBtn
     }
     @objc private func leftItemClick (){
         
     }
     @objc private func rightItemClick () {
+        
+    }
+    func titltBtnClick(btn:TitleButton){
         
     }
 }
