e7ad4b223ff9a82ad962384998bdfbe0e2d3dcb7
醉看红尘这场梦 <15914115336@163.com>
2017-05-05 15:07:31 +0800

    添加动画，等完善

--- Example/Swift项目/微博/微博/Classes/Main/View/VisitorView.swift
+++ Example/Swift项目/微博/微博/Classes/Main/View/VisitorView.swift
@@ -8,14 +8,62 @@
 
 import UIKit
 
+//MARK: - 设置代理方法
+protocol VisitorViewDelegate:NSObjectProtocol {
+    
+    func loginBtnDidClick()
+    
+    func registerBtnClick()
+}
+
 class VisitorView: UIView {
+    
+    weak var delegate: VisitorViewDelegate
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    
+    //MARK: - 访客界面配置，是否有首页，图片名，文字信息
+    func setupVisitorInfo(isHome: Bool, imageName: String, message: String) {
+        
     }
-    */
-
+    
+    //MARK: - 初始化视图
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - 转盘
+    private lazy var iconView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
+        return imageView
+    }()
+    
+    //MARK: - 图标
+    private lazy var homeIcon: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "visitordiscover_feed_image_house"))
+        return image
+    }()
+    
+    //MARK: - 文本
+    private lazy var messageLable: UILabel = {
+        let label = UILabel()
+        label.text = "我是醉看红尘这场梦,这是我仿写的新浪微博项目,用来学习Swift2.0"
+        label.textColor = UIColor.grayColor()
+        label.numberOfLines = 0
+        label.textAlignment = .Center
+        return label
+    }()
+    
+    
+    
+    func loginBtnDidClick() {
+    
+    }
+    
+    func registerBtnClick() {
+    
+    }
 }
