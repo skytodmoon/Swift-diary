2b3f1d0966742dfdc8b30a4b9eba1dc6c11546de
金亮齐 <15914115336@163.com>
2017-05-07 20:04:38 +0800

    设置主框架

--- 项目/Swift项目/斗鱼(Swift)/斗鱼(Swift)/Classes/Main/Controller/MainViewController.swift
+++ 项目/Swift项目/斗鱼(Swift)/斗鱼(Swift)/Classes/Main/Controller/MainViewController.swift
@@ -12,7 +12,12 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //MARK: - 点击时候文字的颜色
+        let tabBarAppear = UITabBarItem.appearance()
+        tabBarAppear.setTitleTextAttributes([NSForegroundColorAttributeName:UIColor.orange], for: UIControlState.selected)
 
+        setUpAllChildViewControllers()
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +26,29 @@ class MainViewController: UITabBarController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - 设置控制器
+    fileprivate func setUpAllChildViewControllers () {
+        let normalImages = ["btn_home_normal","btn_live_normal","btn_column_normal","btn_user_normal"]
+        let selectedImages = ["btn_home_selected","btn_live_selected","btn_column_selected","btn_user_selected"]
+        
+        let newsVC = HomeViewController()
+        setUpNavRootViewCOntrollers(vc: newsVC, title: "首页", imageName: normalImages[0], selImage: selectedImages[0])
+        
+        let circleVC = LiveViewController()
+        setUpNavRootViewCOntrollers(vc: circleVC, title: "直播", imageName: normalImages[1], selImage: selectedImages[1])
+        
+        let garageVC = UIViewController()
+        setUpNavRootViewCOntrollers(vc: garageVC, title: "关注", imageName: normalImages[2], selImage: selectedImages[2])
+        
+        let profileVC = ProfileViewController()
+        setUpNavRootViewCOntrollers(vc: profileVC, title: "我的", imageName: normalImages[3], selImage: selectedImages[3])
     }
-    */
-
+    
+    
+    fileprivate func setUpNavRootViewCOntrollers(vc: UIViewController, title:String, imageName: String, selImage: String) {
+        vc.title = title
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: selImage)?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+        self.addChildViewController(MainNavController(rootViewController: vc))
+    }
 }
