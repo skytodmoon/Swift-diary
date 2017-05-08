28c2617e0fe74b7b374db7e3923b0dcd92df66ae
醉看红尘这场梦 <15914115336@163.com>
2017-05-08 14:38:34 +0800

    添加status

--- 项目/Swift项目/微博/微博/Classes/Home/Model/Status.swift
+++ 项目/Swift项目/微博/微博/Classes/Home/Model/Status.swift
@@ -15,6 +15,55 @@ class Status: NSObject {
     //MARK: - 微博创建时间
     var created_at: String?{
         didSet{
+            let createdDate = NSDate.dateWithStr(created_at!)
+            created_at = createdDate.descDate
         }
+    }
+    //MARK: - 微博id
+    var id: Int = 0
+    //MARK: 微博内容
+    var text: String?
+    //MARK: 微博来源
+    var source: String?{
+        didSet{
+            //MARK: - 截取字符串
+            if let str = source{
+                if str == ""{
+                    return
+                }
+                //MARK: - 获取开始截取的位置
+                let startLocation = (str as NSString).rangeOfString(">").location + 1
+                //MARK: - 获取截取的长度
+                let length = (str as NSString).rangeOfString("<",options: NSStringCompareOptions.BackwardsSearch).location - startLocation
+                //MARK: 截取字符串
+                source = "来自:" + (str as NSString).substringWithRange(NSMakeRange(startLocation, length))
+            }
+        }
+    }
+    //MARK: - 配图数组
+    var pic_urls: [[String: AnyObject]]?{
+        didSet{
+            storedPicURLS = [NSURL]()
+            storedLargePicURLS = [NSURL]()
+            for dict in pic_urls!{
+                if let urlStr = dict["thumbnail_pic"]{
+                    let largeURLStr = urlStr.stringByReplacingOccurrencesOfString("thumbnail", withString: "large")
+                    storedLargePicURLS!.append(NSURL(string: largeURLStr)!)
+                }
+            }
+        }
+    }
+    
+    //MARK: 配图的URL
+    var storedPicURLS: [NSURL]?
+    //MARK: 配图最大的URL
+    var storedLargePicURLS: [NSURL]?
+    //MARK: 用户信息
+    var user: User?
+    //MARK: 转发微博
+    var reteeted_status: Status?
+    //MARK: 配置URL数组
+    var pictureURL:[NSURL]?{
+        return reteeted_status != nil ? reteeted_status?.storedPicURLS : storedPicURLS
     }
 }
--- 项目/Swift项目/微博/微博/Classes/Other/Tools/NSDate+Extension.swift
+++ 项目/Swift项目/微博/微博/Classes/Other/Tools/NSDate+Extension.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-extension NSData {
+extension NSDate {
     
     class func dateWithStr(time: String) ->NSDate {
         
@@ -20,5 +20,37 @@ extension NSData {
         return createdDate
     }
     
-    
+    //MARK: - 刚刚(一分钟内) X分钟前(一小时内) X小时前(当天) 昨天 HH:mm(昨天) MM-dd HH:mm(一年内) yyyy-MM-dd HH:mm(更早期)
+    var descDate: String {
+        let calendar = NSCalendar.currentCalendar()
+        
+        if calendar.isDateInToday(self){
+            let since = Int(NSDate().timeIntervalSinceDate(self))
+            if since < 60 {
+                return "刚刚"
+            }
+            if since < 60 * 60{
+                return "\(since/60)分钟前"
+            }
+            return "\(since / (60 * 60))小时前"
+        }
+        
+        var formatterStr = "HH:mm"
+        if calendar.isDateInYesterday(self){
+            formatterStr = "昨天" + formatterStr
+        }else{
+            formatterStr = "MM-dd" + formatterStr
+            
+            let comps = calendar.components(NSCalendarUnit.Year, fromDate: self, toDate: NSDate(), options: NSCalendarOptions(rawValue: 0))
+            if comps.year >= 1
+            {
+                formatterStr = "yyyy-" + formatterStr
+            }
+        }
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = formatterStr
+        formatter.locale = NSLocale(localeIdentifier: "en")
+        
+        return formatter.stringFromDate(self)
+    }
 }
