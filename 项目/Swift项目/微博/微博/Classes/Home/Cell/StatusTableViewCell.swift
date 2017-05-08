66d78efa6e422a1e444bb3241cb72aa0325e2042
醉看红尘这场梦 <15914115336@163.com>
2017-05-08 11:55:59 +0800

    添加首页cell和view

---
+++ 项目/Swift项目/微博/微博/Classes/Home/Cell/StatusTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StatusTableViewCell.swift
+//  微博
+//
+//  Created by 金亮齐 on 2017/5/8.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+class StatusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ 项目/Swift项目/微博/微博/Classes/Home/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  微博
+//
+//  Created by 金亮齐 on 2017/5/8.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
