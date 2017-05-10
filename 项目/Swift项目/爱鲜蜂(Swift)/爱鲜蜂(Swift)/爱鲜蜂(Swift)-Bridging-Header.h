1f0e3ce82eb6c1a267a28f8403f2ad3e41ddc96e
醉看红尘这场梦 <15914115336@163.com>
2017-05-10 11:11:21 +0800

    添加第三方

--- 项目/Swift项目/爱鲜蜂(Swift)/爱鲜蜂(Swift)/爱鲜蜂(Swift)-Bridging-Header.h
+++ 项目/Swift项目/爱鲜蜂(Swift)/爱鲜蜂(Swift)/爱鲜蜂(Swift)-Bridging-Header.h
@@ -2,7 +2,9 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-
+#import <SVProgressHUD/SVProgressHUD.h>
+#import <SDWebImage/UIImageView+WebCache.h>
+#import <MJRefresh/MJRefresh.h>
 #import "UMSocial.h"
 #import <CoreLocation/CoreLocation.h>
 #import "UMSocialSinaSSOHandler.h"
