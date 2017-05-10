52a9b61b7f3a3a872a6bc4a2d1f7f24781501a50
醉看红尘这场梦 <15914115336@163.com>
2017-05-10 16:14:41 +0800

    完成引导页

--- 项目/Swift项目/爱鲜蜂(Swift)/爱鲜蜂(Swift)/Classes/Main/Controller/GuideViewController.swift
+++ 项目/Swift项目/爱鲜蜂(Swift)/爱鲜蜂(Swift)/Classes/Main/Controller/GuideViewController.swift
@@ -9,27 +9,84 @@
 import UIKit
 
 class GuideViewController: BaseViewController {
+    
+    private var collectionView: UICollectionView?
+    private var imageNames = ["guide_40_1", "guide_40_2", "guide_40_3", "guide_40_4"]
+    private let cellIdentifier = "GuideCell"
+    private var isHiddenNextButton = true
+    private var pageController = UIPageControl(frame: CGRectMake(0, ScreenHeight - 50, ScreenWidth, 20))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        UIApplication.sharedApplication().setStatusBarHidden(false, withAnimation: UIStatusBarAnimation.None)
+        buildCollectionView()
+        
+        buildPageController()
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func buildCollectionView(){
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.itemSize = ScreenBounds.size
+        layout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        
+        collectionView = UICollectionView(frame: ScreenBounds, collectionViewLayout: layout)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.showsHorizontalScrollIndicator = false
+        collectionView?.showsVerticalScrollIndicator = false
+        collectionView?.pagingEnabled = true
+        collectionView?.bounces = false
+        collectionView?.registerClass(GuideCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        view.addSubview(collectionView!)
+    }
+    private func buildPageController(){
+        pageController.numberOfPages = imageNames.count
+        pageController.currentPage = 0
+        view.addSubview(pageController)
+    }
+
+}
+
+//MARK: - UICollectionViewDelegate,UICollectionViewDataSource
+extension GuideViewController: UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return imageNames.count
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as! GuideCell
+        cell.newImage = UIImage(named: imageNames[indexPath.row])
+        if indexPath.row != imageNames.count - 1 {
+            cell.setNextButtonHidden(true)
+        }
+        
+        return cell
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        if scrollView.contentOffset.x == ScreenWidth * CGFloat(imageNames.count - 1) {
+            let cell = collectionView!.cellForItemAtIndexPath(NSIndexPath(forRow: imageNames.count - 1, inSection: 0)) as! GuideCell
+            cell.setNextButtonHidden(false)
+            isHiddenNextButton = false
+        }
     }
-    */
-
-}
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if scrollView.contentOffset.x != ScreenWidth * CGFloat(imageNames.count - 1) && !isHiddenNextButton && scrollView.contentOffset.x > ScreenWidth * CGFloat(imageNames.count - 2) {
+            let cell = collectionView!.cellForItemAtIndexPath(NSIndexPath(forRow: imageNames.count - 1, inSection: 0)) as! GuideCell
+            cell.setNextButtonHidden(true)
+            isHiddenNextButton = true
+        }
+        pageController.currentPage = Int(scrollView.contentOffset.x / ScreenWidth + 0.5)
+        }
+    }
---
+++ 项目/Swift项目/爱鲜蜂(Swift)/爱鲜蜂(Swift)/Classes/Main/View/GuideCell.swift
@@ -0,0 +1,45 @@
+//
+//  GuideCell.swift
+//  爱鲜蜂(Swift)
+//
+//  Created by 金亮齐 on 2017/5/10.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+class GuideCell: UICollectionViewCell {
+    
+    private let newImageView = UIImageView(frame: ScreenBounds)
+    private let nextButton = UIButton(frame: CGRectMake((ScreenWidth - 100) * 0.5, ScreenHeight - 110, 100,33))
+    
+    var newImage: UIImage? {
+        didSet{
+            newImageView.image = newImage
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        newImageView.contentMode = UIViewContentMode.ScaleAspectFill
+        contentView.addSubview(newImageView)
+        
+        nextButton.setBackgroundImage(UIImage(named: "icon_next"), forState: UIControlState.Normal)
+        nextButton.addTarget(self, action: #selector(GuideCell.nextButtonClick), forControlEvents: UIControlEvents.TouchUpInside)
+        nextButton.hidden = true
+        contentView.addSubview(nextButton)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setNextButtonHidden(hidden: Bool) {
+        nextButton.hidden = hidden
+    }
+    
+    func nextButtonClick() {
+        NSNotificationCenter.defaultCenter().postNotificationName(GuideViewControllerDidFinish, object: nil)
+    }
+}
