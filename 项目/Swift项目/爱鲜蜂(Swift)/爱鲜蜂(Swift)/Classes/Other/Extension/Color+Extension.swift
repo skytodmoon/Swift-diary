3f5df1bc261696ed90af6e9399362e38cf65b8a7
醉看红尘这场梦 <15914115336@163.com>
2017-05-10 12:56:56 +0800

    扩展类

--- 项目/Swift项目/爱鲜蜂(Swift)/爱鲜蜂(Swift)/Classes/Other/Extension/Color+Extension.swift
+++ 项目/Swift项目/爱鲜蜂(Swift)/爱鲜蜂(Swift)/Classes/Other/Extension/Color+Extension.swift
@@ -10,6 +10,7 @@ import UIKit
 
 extension UIColor {
     
+    //MARK: - 颜色扩展类设置
     class func colorWithCustom(r: CGFloat, g:CGFloat, b:CGFloat) -> UIColor {
         return UIColor(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: 1)
     }
