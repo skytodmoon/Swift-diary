140febd043efab1c2fc9e559ebcb3558d5858fe4
醉看红尘这场梦 <15914115336@163.com>
2017-05-10 20:35:12 +0800

    提交oc微博项目
    提交oc微博项目

---
+++ 项目/OC项目/微博(OC)/Pods/Target Support Files/Pods-微博(OC)UITests/Pods-微博(OC)UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods____OC_UITests : NSObject
+@end
+@implementation PodsDummy_Pods____OC_UITests
+@end
---
+++ 项目/OC项目/微博(OC)/微博(OC)/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  微博(OC)
+//
+//  Created by 金亮齐 on 2017/5/10.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
