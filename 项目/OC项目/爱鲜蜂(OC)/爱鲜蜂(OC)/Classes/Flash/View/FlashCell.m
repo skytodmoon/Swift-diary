453119076870d7bc1858798b77fd56801485239c
醉看红尘这场梦 <15914115336@163.com>
2017-05-11 19:24:34 +0800

    添加商城view

---
+++ 项目/OC项目/爱鲜蜂(OC)/爱鲜蜂(OC)/Classes/Flash/View/FlashCell.m
@@ -0,0 +1,24 @@
+//
+//  FlashCell.m
+//  爱鲜蜂(OC)
+//
+//  Created by 金亮齐 on 2017/5/11.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import "FlashCell.h"
+
+@implementation FlashCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
