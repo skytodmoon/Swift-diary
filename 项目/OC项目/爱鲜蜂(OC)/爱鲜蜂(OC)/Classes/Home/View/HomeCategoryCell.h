603bd519de769daeb3455caee021d531da8a6ff7
金亮齐 <15914115336@163.com>
2017-05-11 22:28:53 +0800

    提交控制器

---
+++ 项目/OC项目/爱鲜蜂(OC)/爱鲜蜂(OC)/Classes/Home/View/HomeCategoryCell.h
@@ -0,0 +1,13 @@
+//
+//  HomeCategoryCell.h
+//  爱鲜蜂(OC)
+//
+//  Created by 金亮齐 on 2017/5/11.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeCategoryCell : UICollectionViewCell
+
+@end
---
+++ 项目/OC项目/爱鲜蜂(OC)/爱鲜蜂(OC)/Classes/Home/View/HomeCellGoodsView.m
@@ -0,0 +1,21 @@
+//
+//  HomeCellGoodsView.m
+//  爱鲜蜂(OC)
+//
+//  Created by 金亮齐 on 2017/5/11.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import "HomeCellGoodsView.h"
+
+@implementation HomeCellGoodsView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
