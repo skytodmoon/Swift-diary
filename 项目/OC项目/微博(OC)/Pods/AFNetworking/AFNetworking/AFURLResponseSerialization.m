05970812af3c98abef506d8bf4d246835afa4b51
醉看红尘这场梦 <15914115336@163.com>
2017-05-12 16:04:13 +0800

    添加授权

--- 项目/OC项目/微博(OC)/Pods/AFNetworking/AFNetworking/AFURLResponseSerialization.m
+++ 项目/OC项目/微博(OC)/Pods/AFNetworking/AFNetworking/AFURLResponseSerialization.m
@@ -224,9 +224,7 @@ static id AFJSONObjectByRemovingKeysWithNullValues(id JSONObject, NSJSONReadingO
     if (!self) {
         return nil;
     }
-
-    self.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", nil];
-
+    self.acceptableContentTypes =[NSSet setWithObjects:@"application/json",@"text/json",@"text/javascript",@"text/plain",@"text/html", nil];
     return self;
 }
 
