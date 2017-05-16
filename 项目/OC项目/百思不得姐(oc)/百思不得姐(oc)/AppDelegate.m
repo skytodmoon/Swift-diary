815df9639b62cd5e09e81664f1b7884fa7f65f56
醉看红尘这场梦 <15914115336@163.com>
2017-05-16 10:02:57 +0800

    加载网络广告

--- 项目/OC项目/百思不得姐(oc)/百思不得姐(oc)/AppDelegate.m
+++ 项目/OC项目/百思不得姐(oc)/百思不得姐(oc)/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ADViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    //设置MainController为rootViewController
+    self.window.rootViewController = [[ADViewController alloc]init];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
