28bf0d11681038253d9ff77cf8c1b3f6f5e96b33
金亮齐 <15914115336@163.com>
2017-05-16 22:26:52 +0800

    安装第三方库

---
+++ 项目/Swift项目/百思不得姐(Swift)/Pods/Target Support Files/Pods-百思不得姐(Swift)/Pods-百思不得姐(Swift)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_______Swift_ : NSObject
+@end
+@implementation PodsDummy_Pods_______Swift_
+@end
---
+++ 项目/Swift项目/百思不得姐(Swift)/Pods/Target Support Files/Zhugeio/Zhugeio-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Base64.h"
+#import "Compres.h"
+#import "ZGLog.h"
+#import "Zhuge.h"
+#import "ZhugeConfig.h"
+
+FOUNDATION_EXPORT double ZhugeioVersionNumber;
+FOUNDATION_EXPORT const unsigned char ZhugeioVersionString[];
+
