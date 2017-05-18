b1fa3ae0fad4d7544eb4f8d0e3bf24360bbfd966
醉看红尘这场梦 <15914115336@163.com>
2017-05-18 10:23:27 +0800

    添加NSString+Extension

--- 项目/Swift项目/今日头条(Swift)/今日头条(Swift)/Classes/Other/Extension/UIImageView+Extension.swift
+++ 项目/Swift项目/今日头条(Swift)/今日头条(Swift)/Classes/Other/Extension/UIImageView+Extension.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+
+    func setCircleHeader(url: String){
+        let placeholder = UIImage(named: "home_head_default_29x29_")
+        self.kf_setImageWithURL(NSURL(string: url)!)
+        self.kf_setImageWithURL(NSURL(string: url)!, placeholderImage: placeholder, optionsInfo: nil, progressBlock: nil) { (image, error, cacheType, imageURL) in
+            self.image = (image == nil) ? placeholder : image?.circleImage()
+        }
+    }
+}
