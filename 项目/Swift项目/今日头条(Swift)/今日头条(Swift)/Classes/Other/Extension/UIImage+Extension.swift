2aa2801e1d2c8821b23a3f77c67d5fb71ca209c4
醉看红尘这场梦 <15914115336@163.com>
2017-05-18 10:17:34 +0800

    添加图片扩展类

--- 项目/Swift项目/今日头条(Swift)/今日头条(Swift)/Classes/Other/Extension/UIImage+Extension.swift
+++ 项目/Swift项目/今日头条(Swift)/今日头条(Swift)/Classes/Other/Extension/UIImage+Extension.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    
+    func circleImage() -> UIImage {
+        // false 代表透明
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        // 获得上下文
+        let ctx = UIGraphicsGetCurrentContext()
+        // 添加一个圆
+        let rect = CGRectMake(0, 0, size.width, size.height)
+        CGContextAddEllipseInRect(ctx, rect)
+        
+        // 裁剪
+        CGContextClip(ctx)
+        // 将图片画上去
+        drawInRect(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        // 关闭上下文
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+}
