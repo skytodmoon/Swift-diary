6cfa1000ca348f4e91e001f055ee18b495400029
金亮齐 <15914115336@163.com>
2017-05-23 22:08:18 +0800

    提交练习

---
+++ Swift-Example/练习/MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//使用let声明常量，使用var声明变量
+var myVariable = 42
+myVariable = 50
+let myConstans = 42
+
+//没有初始值没有提供的足够信息，那么需要在变量后面声明类型，用冒号分割
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+//值永远不会被隐藏转换为其他类型，如果你需要把一个值转换其他类型，请显示转换
+let label = "The width is"
+let width = 94
+let widthLabel = label + String(width)
+
+//有一种更简单的把值转换成字符串的方法：把值写到括号中，并且在括号之前写一个反斜杠。
+let apples = 3
+let oranges = 5
+let appleSummary = "i have \(apples)apples"
+let fruitSummary = "i have \(apples + oranges) pieces of fruit"
+
