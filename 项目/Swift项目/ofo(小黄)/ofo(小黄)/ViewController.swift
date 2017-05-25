d1b5a5006affbc8f8b42995209c3c97882b71123
金亮齐 <15914115336@163.com>
2017-05-25 23:22:55 +0800

    完成搜索

--- 项目/Swift项目/ofo(小黄)/ofo(小黄)/ViewController.swift
+++ 项目/Swift项目/ofo(小黄)/ofo(小黄)/ViewController.swift
@@ -9,13 +9,105 @@
 import UIKit
 import SWRevealViewController
 
-class ViewController: UIViewController,MAMapViewDelegate {
+class ViewController: UIViewController,MAMapViewDelegate,AMapSearchDelegate {
     
     var mapView: MAMapView!
+    var search: AMapSearchAPI!
+    var pin: MyPinAnnotation!
+    var pinView: MAAnnotationView!
     
     @IBOutlet weak var panelView: UIView!
 
     @IBAction func locationBtnTap(_ sender: UIButton) {
+        searchBikeNearby()
+    }
+    
+    //MARK: - 搜索周边的小黄车请求
+    func searchBikeNearby(){
+        searchCustomLocation(mapView.userLocation.coordinate)
+    }
+    func searchCustomLocation(_ center: CLLocationCoordinate2D) {
+        let request = AMapPOIAroundSearchRequest()
+        request.location = AMapGeoPoint.location(withLatitude: CGFloat(center.latitude), longitude: CGFloat(center.longitude))
+        request.keywords = "餐馆"
+        request.radius = 500
+        request.requireExtension = true
+        
+        search.aMapPOIAroundSearch(request)
+    }
+    //MARK: - 地图初始化
+    func mapInitComplete(_ mapView: MAMapView!) {
+        pin = MyPinAnnotation()
+        pin.coordinate = mapView.centerCoordinate
+        pin.lockedScreenPoint = CGPoint(x: view.bounds.width/2, y: view.bounds.height/2)
+        pin.isLockedToScreen = true
+        
+        mapView.addAnnotation(pin)
+        mapView.showAnnotations([pin], animated: true)
+    }
+    
+    
+    //MARK: - MapViewDelegate
+    func mapView(_ mapView: MAMapView!, viewFor annotation: MAAnnotation!) -> MAAnnotationView! {
+        //用户定义的位置，不需要自定义
+        if annotation is MAUserLocation {
+            return nil
+        }
+        
+        if annotation is MyPinAnnotation {
+            let reuseid = "anchor"
+            var av = mapView.dequeueReusableAnnotationView(withIdentifier: reuseid)
+            if av == nil {
+                av = MAPinAnnotationView(annotation: annotation, reuseIdentifier: reuseid)
+            }
+            
+            av?.image = #imageLiteral(resourceName: "homePage_wholeAnchor")
+            av?.canShowCallout = false
+            
+            pinView = av
+            return av
+        }
+        
+        let reuseid = "myid"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseid) as? MAPinAnnotationView
+        if annotationView == nil {
+            annotationView = MAPinAnnotationView(annotation: annotation, reuseIdentifier: reuseid)
+        }
+        
+        if annotation.title == "正常可用" {
+            annotationView?.image = #imageLiteral(resourceName: "HomePage_nearbyBike")
+        }else {
+            annotationView?.image = #imageLiteral(resourceName: "HomePage_nearbyBikeRedPacket")
+        }
+        annotationView?.canShowCallout = true
+        annotationView?.animatesDrop = true
+        return annotationView
+    }
+    
+    //MARK: - 搜索周边后完成的处理
+    func onPOISearchDone(_ request: AMapPOISearchBaseRequest!, response: AMapPOISearchResponse!){
+        if response.count == 0 {
+            print("没有小黄车")
+            return
+        }
+        var annotations : [MAPointAnnotation] = []
+        
+        annotations = response.pois.map{
+            let annotation = MAPointAnnotation()
+            
+            annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees($0.location.latitude), longitude: CLLocationDegrees($0.location.longitude))
+            
+            if $0.distance < 400 {
+                annotation.title = "红包区域内开锁任意小黄车"
+                annotation.subtitle = "骑行10分钟获得任意现金红包"
+            }else{
+                annotation.title = "正常可用"
+            }
+            return annotation
+        }
+        
+        mapView.addAnnotations(annotations)
+        mapView.showAnnotations(annotations, animated: true)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,9 +117,14 @@ class ViewController: UIViewController,MAMapViewDelegate {
         view.bringSubview(toFront: panelView)
         
         mapView.delegate = self
-        
+        mapView.zoomLevel = 17
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
+        
+        
+        search = AMapSearchAPI()
+        search.delegate = self
+
 
         setNav()
         setupUI()
