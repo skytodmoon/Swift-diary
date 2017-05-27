dbb327a51958c0fb3f6ee09f8be9a604337cd60b
金亮齐 <15914115336@163.com>
2017-05-27 21:50:48 +0800

    添加二维码

--- 项目/Swift项目/ofo(小黄)/ofo(小黄)/ScanViewController.swift
+++ 项目/Swift项目/ofo(小黄)/ofo(小黄)/ScanViewController.swift
@@ -9,22 +9,61 @@
 import UIKit
 import swiftScan
 
-class ScanViewController: UIViewController {
+class ScanViewController: LBXScanViewController {
+    
+    var isFlashOn = false
+    
+    @IBOutlet weak var flashBtn: UIButton!
+    @IBAction func flashBtnTap(_ sender: UIButton) {
+        
+        isFlashOn = !isFlashOn
+        
+        scanObj?.changeTorch()
+        
+        if isFlashOn {
+            flashBtn.setImage(#imageLiteral(resourceName: "btn_enableTorch_w"), for: .normal)
+        }else{
+            flashBtn.setImage(#imageLiteral(resourceName: "btn_unenableTorch_w"), for: .normal)
+        }
+    }
+    @IBOutlet weak var panelView: UIView!
+    
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        view.bringSubview(toFront: panelView)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationController?.navigationBar.barStyle = .default
+        navigationController?.navigationBar.tintColor = UIColor.black
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "扫码用车"
         navigationController?.navigationBar.barStyle = .blackTranslucent
         navigationController?.navigationBar.tintColor = UIColor.white
+        
+        var style = LBXScanViewStyle()
+        style.anmiationStyle = .NetGrid
+        style.animationImage = UIImage(named: "CodeScan.bundle/qrcode_scan_part_net")
+        
+        scanStyle = style
         // Do any additional setup after loading the view.
+        
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
 
     /*
     // MARK: - Navigation
