88229d0a485f8920b11ef473d8d6e312488fa7e1
金亮齐 <15914115336@163.com>
2017-05-28 21:40:24 +0800

    控制流

--- Swift-Example/练习/MyPlayground.playground/Contents.swift
+++ Swift-Example/练习/MyPlayground.playground/Contents.swift
@@ -25,3 +25,64 @@ let oranges = 5
 let appleSummary = "i have \(apples)apples"
 let fruitSummary = "i have \(apples + oranges) pieces of fruit"
 
+//使用方括号[]来创建数组和字典，并使用下标key来访问元素。最后一个元素后面允许有一个逗号
+var shoppingList  = ["catfish", "water", "tulips", "blue paint"]
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+    "Malcolm":"Caption",
+    "Kaylee":"Mechanic",
+]
+occupations["Jayne"] = "Public Relations"
+
+//创建一个空数组活着字典，使用初始化语法
+let emptyArray = [String]()
+let emptyDictionary = [String: Float]()
+
+//如果类型信息可以被推断出来，你可以用[]和[:]来创建空数组和空字典
+shoppingList = []
+occupations = [:]
+
+//=========================控制流======================
+//if switch for-in for while ,包裹条条件循环变量括号可以省略
+//但是语句体的大括号是必须的
+let individualScores = [75,43,103,87,12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    }else{
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+
+//一个可选的值是一个具体的值或者nil以表示缺少，在类型后面加上一个
+//问号来标记这个变量的值是可选的
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = "John Applessed"
+var greeting = "Hello!"
+if let name = optionalName{
+    greeting = "Hello,\(name)"
+}
+
+//另一种处理可选的方法是通过使用??操作符来提供一个默认值
+let nickName: String? = nil
+let fullName: String = "John Applessed"
+let informalGreeting = "Hi\(nickName ?? fullName)"
+
+//switch支持任意类型的数据以及各种比较操作
+let vegetable = "red pepper"
+switch vegetable {
+    case "celery":
+        print("Add some raisins and make ants on a log")
+    case "cucumber","watercress":
+        print("That would make a good tea sandwich")
+case let x where x.hasSuffix("pepper"):
+        print("is it a spicy\(x)?")
+default:
+    print("Everything tastes good in soup")
+}
