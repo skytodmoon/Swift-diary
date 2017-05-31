afa4408e9b355ced0522734d337e68e7b12d8747
金亮齐 <15914115336@163.com>
2017-05-31 23:13:09 +0800

    提交扩展类

---
+++ 项目/Swift项目/小黄车(Swift)/小黄车(Swift)/Classes/小黄车(Swift)-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <CommonCrypto/CommonDigest.h>
+#import <CommonCrypto/CommonHMAC.h>
+#import <CommonCrypto/CommonCrypto.h>
+
+// 侧边栏
+//#import "SWRevealViewController.h"
+// #import "CDAudioManager.h"
+
+// 地图
+#import <MAMapKit/MAMapKit.h>
+#import <AMapFoundationKit/AMapFoundationKit.h>
+#import <AMapLocationKit/AMapLocationKit.h>
+#import <AMapSearchKit/AMapSearchKit.h>
+#import <AMapNaviKit/AMapNaviKit.h>
