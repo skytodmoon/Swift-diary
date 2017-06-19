0e95a24d2f138dec1d950a9c031c5958e67b8b8c
qijinliang <15914115336@163.com>
2017-06-19 09:19:31 +0800

    提交扩展类

---
+++ 项目/OC项目/全民TV/全民TV/Classes/Vendors/HUD+Toast/NSObject+HUD.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+HUD.h
+//  全民TV
+//
+//  Created by 金亮齐 on 2017/6/19.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (HUD)
+- (void)showInfo:(NSString *)info;
+@end
