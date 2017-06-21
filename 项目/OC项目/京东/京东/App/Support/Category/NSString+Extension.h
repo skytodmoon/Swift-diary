eb56eaa14ca8485e342bd1de8612f28ca35f0ffe
qijinliang <15914115336@163.com>
2017-06-21 11:25:32 +0800

    修改项目架构

---
+++ 项目/OC项目/京东/京东/App/Support/Category/NSString+Extension.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Extension.h
+//  京东
+//
+//  Created by 金亮齐 on 2017/6/21.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Extension)
+- (CGSize)sizeWithFont:(UIFont *)font maxSize:(CGSize)maxSize;
+@end
