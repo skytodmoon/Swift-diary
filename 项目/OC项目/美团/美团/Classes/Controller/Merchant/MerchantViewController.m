d77d72ab95092fdeb7b1f843992758b644574df1
qijinliang <15914115336@163.com>
2017-06-30 15:54:27 +0800

    封装view

---
+++ 项目/OC项目/美团/美团/Classes/Controller/Merchant/MerchantViewController.m
@@ -0,0 +1,198 @@
+//
+//  MerchantViewController.m
+//  美团
+//
+//  Created by 金亮齐 on 2017/6/30.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import "MerchantViewController.h"
+
+@interface MerchantViewController ()<UITableViewDelegate,UITableViewDataSource>{
+    NSInteger _offset;
+}
+
+@property(strong,nonatomic)UITableView *tableView;
+
+@end
+
+@implementation MerchantViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationController.navigationBarHidden = YES;
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self setupNav];
+    [self initView];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+//MARK: - 设置导航栏
+-(void)setupNav{
+    UIView *backView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screen_width, 64)];
+    backView.backgroundColor = RGB(250, 250, 250);
+    [self.view addSubview:backView];
+    
+    //下划线
+    UIView *lineview = [[UIView alloc]initWithFrame:CGRectMake(0, 63.5, screen_width, 0.5)];
+    lineview.backgroundColor = RGB(192, 192, 192);
+    [self.view addSubview:lineview];
+    
+    //地图按钮
+    UIButton *mapBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    mapBtn.frame = CGRectMake(10, 30, 23, 23);
+    [mapBtn setImage:[UIImage imageNamed:@"icon_map"] forState:UIControlStateNormal];
+    [mapBtn addTarget:self action:@selector(OnmapBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [backView addSubview:mapBtn];
+    
+    //搜索按钮
+    UIButton *searchBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    searchBtn.frame = CGRectMake(screen_width-42, 30, 23, 23);
+    [searchBtn setImage:[UIImage imageNamed:@"icon_search"] forState:UIControlStateNormal];
+    [searchBtn addTarget:self action:@selector(OnSearchBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [backView addSubview:searchBtn];
+    
+    //左右分页
+    UIButton *segBtn1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    segBtn1.frame = CGRectMake(screen_width/2-80, 30, 80, 30);
+    segBtn1.tag = 20;
+    [segBtn1 setTitle:@"全部商家" forState:UIControlStateNormal];
+    [segBtn1 setTitleColor:[UIColor whiteColor] forState:UIControlStateSelected];
+    [segBtn1 setTitleColor:navigationBarColor forState:UIControlStateNormal];
+    [segBtn1 setBackgroundColor:navigationBarColor];
+    segBtn1.selected = YES;
+    segBtn1.font = [UIFont systemFontOfSize:15];
+    segBtn1.layer.borderWidth = 1;
+    segBtn1.layer.borderColor = [navigationBarColor CGColor];
+    [segBtn1 addTarget:self action:@selector(OnSegBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [backView addSubview:segBtn1];
+    
+    UIButton *segBtn2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    segBtn2.frame = CGRectMake(screen_width/2, 30, 80, 30);
+    segBtn2.tag = 21;
+    [segBtn2 setTitle:@"优惠商家" forState:UIControlStateNormal];
+    [segBtn2 setTitleColor:[UIColor whiteColor] forState:UIControlStateSelected];
+    [segBtn2 setTitleColor:navigationBarColor forState:UIControlStateNormal];
+    [segBtn2 setBackgroundColor:[UIColor whiteColor]];
+    segBtn2.font = [UIFont systemFontOfSize:15];
+    segBtn2.layer.borderWidth = 1;
+    segBtn2.layer.borderColor = [navigationBarColor CGColor];
+    [segBtn2 addTarget:self action:@selector(OnSegBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [backView addSubview:segBtn2];
+    
+
+}
+
+//MARK: - 设置UI
+-(void)initView{
+    //刷选
+    UIView *filterView = [[UIView alloc]initWithFrame:CGRectMake(0, 64, screen_width, 40)];
+    filterView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:filterView];
+    
+    NSArray *filterName = @[@"全部",@"全部",@"智能排序"];
+    for (int i = 0; i<3; i++) {
+        UIButton *filterBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        filterBtn.frame = CGRectMake(i*screen_width/3, 0, screen_width/3-15, 40);
+        filterBtn.tag =100+i;
+        filterBtn.font = [UIFont systemFontOfSize:13];
+        [filterBtn setTitle:filterName[i] forState:UIControlStateNormal];
+        [filterBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [filterBtn setTitleColor:navigationBarColor forState:UIControlStateSelected];
+        [filterBtn addTarget:self action:@selector(OnFilterBtn:) forControlEvents:UIControlEventTouchUpInside];
+        [filterView addSubview:filterBtn];
+        //三角图标
+        UIButton *sanjiaoBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        sanjiaoBtn.frame = CGRectMake((i+1)*screen_width/3-15, 16, 8, 7);
+        sanjiaoBtn.tag = 120+i;
+        [sanjiaoBtn setImage:[UIImage imageNamed:@"icon_arrow_dropdown_normal"] forState:UIControlStateNormal];
+        [sanjiaoBtn setImage:[UIImage imageNamed:@"icon_arrow_dropdown_selected"] forState:UIControlStateSelected];
+        [filterView addSubview:sanjiaoBtn];
+    }
+    //下划线
+    UIView *lineView = [[UIView alloc]initWithFrame:CGRectMake(0, 39.5, screen_width, 0.5)];
+    lineView.backgroundColor = RGB(192, 192, 192);
+    [filterView addSubview:lineView];
+    
+    self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 64+40, screen_width, screen_height-64-40-49) style:UITableViewStylePlain];
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self.view addSubview:self.tableView];
+    [self setUpTableView];
+    
+}
+
+//MARK: - 上下拉刷新
+-(void)setUpTableView{
+    //下拉刷新 在开始刷新后会调用此block
+    self.tableView.mj_header = [RefreshHeader headerWithRefreshingBlock:^{
+        [self getFirstPageData];
+        //结束刷新
+        [self.tableView.mj_header endRefreshing];
+    }];
+    
+    //上拉刷新 在开始刷新后会调用此block
+    self.tableView.mj_footer = [MJRefreshFooter footerWithRefreshingBlock:^{
+        [self loadMoreData];
+        [self.tableView.mj_footer endRefreshing];
+    }];
+}
+
+#pragma mark - 点击事件
+
+-(void)OnmapBtn:(UIButton *)sender{
+    NSLog(@"地图");
+}
+
+-(void)OnSearchBtn:(UIButton *)sender{
+    NSLog(@"搜索");
+}
+
+
+-(void)OnSegBtn:(UIButton *)sender{
+    NSInteger tag = sender.tag;
+    UIButton *segBtn1 = (UIButton *)[self.view viewWithTag:20];
+    UIButton *segBtn2 = (UIButton *)[self.view viewWithTag:21];
+    [segBtn1 setBackgroundColor:[UIColor whiteColor]];
+    [segBtn2 setBackgroundColor:[UIColor whiteColor]];
+    segBtn1.selected = NO;
+    segBtn2.selected = NO;
+    sender.selected = YES;
+    [sender setBackgroundColor:navigationBarColor];
+    if (tag ==20) {
+        
+    }else{
+        
+    }
+ 
+}
+-(void)OnFilterBtn:(UIButton *)sender{
+    
+}
+
+#pragma mark - 请求数据
+-(void)getFirstPageData{
+    _offset =0;
+    [self refreshData];
+}
+
+-(void)loadMoreData{
+    
+}
+
+-(void)refreshData{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self getMerchantList];
+    });
+}
+
+//MARK: - 获取商家列表
+-(void)getMerchantList{
+    
+}
+@end
