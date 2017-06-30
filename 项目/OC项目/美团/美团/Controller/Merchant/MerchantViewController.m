98d8c35ac1a19be7e5d2f5368cbdb03e99729086
qijinliang <15914115336@163.com>
2017-06-30 12:59:32 +0800

    添加左右分页

--- 项目/OC项目/美团/美团/Controller/Merchant/MerchantViewController.m
+++ 项目/OC项目/美团/美团/Controller/Merchant/MerchantViewController.m
@@ -19,6 +19,8 @@
     
     self.navigationController.navigationBarHidden = YES;
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self setupNav];
     // Do any additional setup after loading the view.
 }
 
@@ -26,15 +28,59 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+//MARK: - 设置导航栏
+-(void)setupNav{
+    UIView *backView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screen_width, 64)];
+    backView.backgroundColor = RGB(250, 250, 250);
+    [self.view addSubview:backView];
+    
+    //下划线
+    UIView *lineview = [[UIView alloc]initWithFrame:CGRectMake(0, 63.5, screen_width, 0.5)];
+    lineview.backgroundColor = RGB(192, 192, 192);
+    [self.view addSubview:lineview];
+    
+    //地图按钮
+    UIButton *mapBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    mapBtn.frame = CGRectMake(10, 30, 23, 23);
+    [mapBtn setImage:[UIImage imageNamed:@"icon_map"] forState:UIControlStateNormal];
+    [mapBtn addTarget:self action:@selector(OnmapBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [backView addSubview:mapBtn];
+    
+    //搜索按钮
+    UIButton *searchBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    searchBtn.frame = CGRectMake(screen_width-42, 30, 23, 23);
+    [searchBtn setImage:[UIImage imageNamed:@"icon_search"] forState:UIControlStateNormal];
+    [searchBtn addTarget:self action:@selector(OnSearchBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [backView addSubview:searchBtn];
+    
+    //左右分页
+    UIButton *segBtn1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    segBtn1.frame = CGRectMake(screen_width/2-80, 30, 80, 30);
+    segBtn1.tag = 20;
+    [segBtn1 setTitle:@"全部商家" forState:UIControlStateNormal];
+    [segBtn1 setTitleColor:[UIColor whiteColor] forState:UIControlStateSelected];
+    [segBtn1 setTitleColor:navigationBarColor forState:UIControlStateNormal];
+    segBtn1.selected = YES;
+    segBtn1.font = [UIFont systemFontOfSize:15];
+    segBtn1.layer.borderWidth = 1;
+    segBtn1.layer.borderColor = [navigationBarColor CGColor];
+    [segBtn1 addTarget:self action:@selector(OnSegBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [backView addSubview:segBtn1];
+    
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-*/
 
+#pragma mark - 点击事件
+
+-(void)OnmapBtn:(UIButton *)sender{
+    NSLog(@"地图");
+}
+
+-(void)OnSearchBtn:(UIButton *)sender{
+    NSLog(@"搜索");
+}
+
+-(void)OnSegBtn:(UIButton *)sender{
+    
+}
 @end
