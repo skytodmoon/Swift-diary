d8bb0147f5cf50e2e09cb441888ab24d4e654de6
qijinliang <15914115336@163.com>
2017-07-19 11:38:36 +0800

    添加分享

---
+++ 项目/OC项目/美团/美团/Classes/View/Shop/ShopPriceCell.h
@@ -0,0 +1,16 @@
+//
+//  ShopPriceCell.h
+//  美团
+//
+//  Created by 金亮齐 on 2017/7/19.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShopPriceCell : UITableViewCell
+
+@property(nonatomic, strong) UILabel *priceLabel;
+@property(nonatomic, strong) UILabel *oldPriceLabel;
+
+@end
