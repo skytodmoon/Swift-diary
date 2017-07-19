a0b1d05aaef03914af15688d493acc0405879c2e
qijinliang <15914115336@163.com>
2017-07-19 17:13:44 +0800

    添加煤美团ipad香满园

---
+++ 项目/OC项目/美团iPad/美团iPad/Classes/Main/Controller/BasicCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  BasicCollectionViewController.h
+//  美团iPad
+//
+//  Created by 金亮齐 on 2017/7/19.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BasicCollectionViewController : UICollectionViewController
+
+@end
---
+++ 项目/OC项目/美团iPad/美团iPad/Classes/Main/Controller/MainController.m
@@ -0,0 +1,13 @@
+//
+//  MainController.m
+//  美团iPad
+//
+//  Created by 金亮齐 on 2017/7/19.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+#import "MainController.h"
+
+@implementation MainController
+
+@end
