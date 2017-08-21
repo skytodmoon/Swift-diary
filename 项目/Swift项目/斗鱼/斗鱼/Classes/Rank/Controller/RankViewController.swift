aeb2eb980030e79f830fa001a9911b3b3cf86773
qijinliang <15914115336@163.com>
2017-08-21 11:20:49 +0800

    添加排行控制器2

--- 项目/Swift项目/斗鱼/斗鱼/Classes/Rank/Controller/RankViewController.swift
+++ 项目/Swift项目/斗鱼/斗鱼/Classes/Rank/Controller/RankViewController.swift
@@ -12,24 +12,36 @@ class RankViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.navigationController?.isNavigationBarHidden = true
+        setupUI()
+        
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension RankViewController {
+    fileprivate func setupUI() {
+        let rect = CGRect(x: 0, y: kStatusBarH, width: kScreenW, height: kScreenH - kStatusBarH)
+        
+        let titles = ["明星榜","富豪榜","人气榜","周星榜"]
+        
+        let style = TitleStyle()
+        style.isShowBottomLine = true
+        style.isScrollEnable = false
+        
+        var childVCs = [SubbankViewController]()
+        for i in 0..<titles.count {
+            
+            let vc = i == 3 ? WeeklyRankViewController() : NomalRankViewController()
+            vc.currentIndex = i
+            childVCs.append(vc)
+            
+        }
+        
+        let scrollPageView = LiveScrollPageView(frame: rect, titles: titles, style: style, childVcs: childVCs, parentVc: self)
+        
+        view.addSubview(scrollPageView)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
