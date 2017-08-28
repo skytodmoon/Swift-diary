13bd168c782baa443a487e758661709edad9f4e7
qijinliang <15914115336@163.com>
2017-08-28 14:51:36 +0800

    添加直播view

--- 项目/Swift项目/斗鱼/斗鱼/Classes/Room/View/ChatContentView/ChatContentView.swift
+++ 项目/Swift项目/斗鱼/斗鱼/Classes/Room/View/ChatContentView/ChatContentView.swift
@@ -8,14 +8,50 @@
 
 import UIKit
 
+private let kChatContentViewID = "kChatContentViewID"
+
 class ChatContentView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var tableView: UITableView!
+    
+    // 定义一个数组,用来保存所有的 message 消息
+    fileprivate lazy var messages: [NSAttributedString] = [NSAttributedString]()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // 注册 cell
+        
+        tableView.register(UINib(nibName: "XJChatContentViewCell", bundle: nil), forCellReuseIdentifier: kChatContentViewID)
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.showsHorizontalScrollIndicator = false
+        tableView.showsVerticalScrollIndicator = false
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
-    */
+    
+    
+    // MARK: 给外界暴露一个函数,将外界传入的数据保存到数组当中
+    func insertMessage(_ message : NSAttributedString) {
+        messages.append(message)
+        tableView.reloadData()
+        // 将数据滚动到最底部
+        let indexPath = IndexPath(row: messages.count - 1, section: 0)
+        tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+    }
+    
+}
 
+extension ChatContentView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kChatContentViewID, for: indexPath) as! ChatContentViewCell
+        cell.contentLabel.attributedText = messages[indexPath.row]
+        
+        
+        return cell
+    }
 }
--- 项目/Swift项目/斗鱼/斗鱼/Classes/Room/View/GiftView/GiftViewCell.swift
+++ 项目/Swift项目/斗鱼/斗鱼/Classes/Room/View/GiftView/GiftViewCell.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class GiftViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var subjectLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
+    var giftModel : GiftModel? {
+        didSet {
+            iconImageView.setImage(giftModel?.img2, UIImage(named: "room_btn_gift"), true)
+            subjectLabel.text = giftModel?.subject
+            priceLabel.text = "\(giftModel?.coin ?? 0)"
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        let selectedView = UIView()
+        selectedView.layer.cornerRadius = 5
+        selectedView.layer.masksToBounds = true
+        selectedView.layer.borderColor = UIColor.orange.cgColor
+        selectedView.layer.borderWidth = 1
+        selectedView.backgroundColor = UIColor.black
+        
+        selectedBackgroundView = selectedView
     }
-
 }
