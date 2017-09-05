1c248dc7b533a8a6fdd36a69f6b84af315f9de81
qijinliang <15914115336@163.com>
2017-09-05 11:47:18 +0800

    创建全民项目，使用Swift4.0

---
+++ 项目/Swift项目/全民/全民/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  全民
+//
+//  Created by 金亮齐 on 2017/9/5.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
