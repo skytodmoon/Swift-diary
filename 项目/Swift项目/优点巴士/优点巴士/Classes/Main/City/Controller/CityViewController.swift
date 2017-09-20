92045a07c025a37d71563989da5743b46c45e4f1
qijinliang <15914115336@163.com>
2017-09-20 10:19:46 +0800

    包车控制器
    包车控制器

--- 项目/Swift项目/优点巴士/优点巴士/Classes/Main/City/Controller/CityViewController.swift
+++ 项目/Swift项目/优点巴士/优点巴士/Classes/Main/City/Controller/CityViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 import ZHDropDownMenu
 
+private let CityCellID = "CityCellID"
+
 class CityViewController: UIViewController {
+    
     
     fileprivate lazy var HeaderView : UIView = {[unowned self] in
         
@@ -25,15 +28,49 @@ class CityViewController: UIViewController {
 
         return headerview
     }()
+    
+    fileprivate lazy var tableView : UITableView = {[unowned self] in
+        let rect = CGRect(x: 0, y: 44, width: ScreenW, height: ScreenH-64-44-44)
+        let tableView = UITableView(frame: rect)
+        tableView.backgroundColor = UIColor.groupTableViewBackground
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+        tableView.register(UINib(nibName: "CityTableViewCell", bundle: nil), forCellReuseIdentifier: CityCellID)
+        return tableView
+        }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor.white
         self.view.addSubview(HeaderView)
+        self.view.addSubview(tableView)
         // Do any additional setup after loading the view.
     }
 
 
 
+}
+
+
+// MARK:- 遵守UITableView的数据源&代理
+extension CityViewController : UITableViewDelegate,UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: CityCellID, for: indexPath) as! CityTableViewCell
+        cell.selectionStyle = UITableViewCellSelectionStyle.none
+        return cell
+    }
+        
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
 }
---
+++ 项目/Swift项目/优点巴士/优点巴士/Classes/Main/City/View/CityTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  CityTableViewCell.swift
+//  优点巴士
+//
+//  Created by 金亮齐 on 2017/9/20.
+//  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
+//
+
+import UIKit
+
+class CityTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var PriceBtn: UIButton!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        PriceBtn.layer.borderColor = UIColor(red: 51/255, green: 145/255, blue: 232/255, alpha: 1.0).cgColor;
+        PriceBtn.layer.borderWidth = 1;
+        PriceBtn.layer.cornerRadius = 10;
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
