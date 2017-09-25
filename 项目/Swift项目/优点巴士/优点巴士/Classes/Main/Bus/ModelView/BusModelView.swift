f241d993d7a96a2f7278acd9dc23c935b6f2f4d7
qijinliang <15914115336@163.com>
2017-09-25 16:30:50 +0800

    测试

--- 项目/Swift项目/优点巴士/优点巴士/Classes/Main/Bus/ModelView/BusModelView.swift
+++ 项目/Swift项目/优点巴士/优点巴士/Classes/Main/Bus/ModelView/BusModelView.swift
@@ -22,7 +22,7 @@ extension BusModelView {
     func loadBusData(_ finishedCallback: @escaping () -> ()) {
         
         Alamofire.request("http://www.youdianbus.cn/ydbus-api/api/user/list_recommand_line?dev_id=D781FAF8-C667-4FBA-B2FE-49E9B21F28C4&lat=22.548081&lng=113.955705&token=a43acba1c90752f93e51f64364b71d9c&user_id=7c19f276d626928a611e0f58eeaddc09", method: .get, parameters: nil).responseJSON { (response) in
-            
+            print(response)
             if let value = response.result.value {
                 let dict = JSON(value)
                 let dataDict = dict["data"].dictionary
