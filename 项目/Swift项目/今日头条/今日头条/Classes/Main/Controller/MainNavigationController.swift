bee1baeac5f284c4c8a8398e0da78fe91684c8e7
qijinliang <15914115336@163.com>
2017-09-27 16:16:47 +0800

    请求我的借口

--- 项目/Swift项目/今日头条/今日头条/Classes/Main/Controller/MainNavigationController.swift
+++ 项目/Swift项目/今日头条/今日头条/Classes/Main/Controller/MainNavigationController.swift
@@ -12,7 +12,12 @@ class MainNavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let navBar = UINavigationBar.appearance()
+        navBar.barTintColor = UIColor.white
+        navBar.tintColor = UIColor(r: 0, g: 0, b: 0, alpha: 0.7)
+        navBar.titleTextAttributes = [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 17)]
+        
+        initGlobalPan()
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +26,34 @@ class MainNavigationController: UINavigationController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        if viewControllers.count > 0 {
+            viewController.hidesBottomBarWhenPushed = true
+            viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "lefterbackicon_titlebar_24x24_"), style: .plain, target: self, action: #selector(navigationBack))
+        }
+        super.pushViewController(viewController, animated: true)
     }
-    */
+    
+    @objc private func navigationBack() {
+        popViewController(animated: true)
+    }
 
+}
+
+//MARK: -  全局手势返回
+extension MainNavigationController:UIGestureRecognizerDelegate {
+    /// 全局拖拽手势
+    fileprivate func initGlobalPan(){
+        // 1.创建Pan手势
+        let target = interactivePopGestureRecognizer?.delegate
+        let globalPan = UIPanGestureRecognizer(target: target, action: Selector(("handleNavigationTransition:")))
+        globalPan.delegate = self
+        self.view.addGestureRecognizer(globalPan)
+        // 2.禁止系统的手势
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+        
+    }
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return self.childViewControllers.count != 1
+    }
 }
