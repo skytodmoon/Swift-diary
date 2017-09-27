f50b43f7a690f3dcd46863436e3867577a32b4da
qijinliang <15914115336@163.com>
2017-09-27 15:33:32 +0800

    扩展类编写

--- 项目/Swift项目/今日头条/今日头条/Classes/Other/Category/UIImage+Extension.swift
+++ 项目/Swift项目/今日头条/今日头条/Classes/Other/Category/UIImage+Extension.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2017年 醉看红尘这场梦. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    /// 颜色转图片
+    class func getImageWithColor(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+    
+}
