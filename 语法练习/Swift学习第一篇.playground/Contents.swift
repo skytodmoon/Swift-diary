b561133bf0c2b75c25133eff995119195f9f1aff
qijinliang <15914115336@163.com>
2017-10-09 09:53:34 +0800

    内容
    内容

---
+++ 语法练习/Swift学习第一篇.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//常量和变量
+
+/* 允许的最大尝试登录次数 */
+let maximumNumberOfLoginAttempts = 10
+/* 当前尝试登录次数 */
+var cyrrentLoginAttempt = 0
+
+var x = 1.0, y = 2.0, z = 3.0
+var a = 12, b = 22, c = 32
+
+//类型的标注
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+
+//常量和变量的命名
+let 你好 = "你好世界"
+let MM = "dogcow"
+let π = 3.14
+
+var friedlyWelcome = "Hello!"
+friedlyWelcome = "BonJour!"
+
+let languageName = "Swift"
+//会报错，languageName不可改变
+//languageName = "Swift++"
+
+//输出常量和变量
+print(friedlyWelcome)
+
+print("这是一个输出\(friedlyWelcome)")
+
+//注释
+
+
+//分号
+let cat = "?";print(cat);var title : String
+
+//整数范围
+//minValue 为 0，是UInt8 类型
+let minValue = UInt8.min
+//maxValue 为 255，是UInt8 类型
+let maxValue = UInt8.max
+
+
+
