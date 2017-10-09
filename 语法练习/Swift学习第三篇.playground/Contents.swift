4cdbd2cc4919b8eb1f505b9bb1993fef123111be
qijinliang <15914115336@163.com>
2017-10-09 13:46:47 +0800

    第三篇
    第三篇

---
+++ 语法练习/Swift学习第三篇.playground/Contents.swift
@@ -0,0 +1,6 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//类和结构体定义
+
--- 语法练习/Swift学习第二篇.playground/Contents.swift
+++ 语法练习/Swift学习第二篇.playground/Contents.swift
@@ -80,5 +80,45 @@ Welcome.insert("!", at: Welcome.endIndex)
 Welcome.insert(contentsOf: "there".characters, at: Welcome.endIndex)
 
 //字符串/字符相等
+let qutation = "we're a lot alike, you and I"
+let sameQuotation = "we're a lot alike, you and I"
+if qutation == sameQuotation {
+    print("These two strings are considered equal")
+}
 
+//前缀/后缀相等
+let remoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
+
+//hasPrefix(_:) 方法来计算话剧中第一幕的场景数
+var act1SceneCount = 0
+for scene in remoAndJuliet {
+    if scene.hasPrefix("Act 1 ") {
+//        ++act1SceneCount
+    }
+}
+print("There are \(act1SceneCount) scenes in Act 1")
+
+//hasSuffix(_:) 方法来计算发生在不同地方的场景数：
+var mansionCount = 0
+var cellCount = 0
+for scene in remoAndJuliet{
+    if scene.hasPrefix("Caplet's mansion"){
+//        ++mansionCount
+    }else if scene.hasPrefix("Frial Lawrence's cell"){
+//        ++cellCount
+    }
+}
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
 
