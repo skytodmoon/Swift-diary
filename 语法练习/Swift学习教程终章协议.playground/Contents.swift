965703dce7ee8aaf339eaea1a68d0becfe027abd
qijinliang <15914115336@163.com>
2017-10-10 09:51:04 +0800

    协议

--- 语法练习/Swift学习教程终章协议.playground/Contents.swift
+++ 语法练习/Swift学习教程终章协议.playground/Contents.swift
@@ -22,4 +22,175 @@ struct Person: FullyNamed{
 let john = Person(fullName: "John Appleseed")
 //john.fullName 为 "John Appleseed"
 
+class Starship: FullyNamed {
+    
+    var prefix: String?
+    var name: String
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+    var fullName: String {
+        return (prefix != nil ? prefix! + "" : "") + name
+    }
+}
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+
+//对方法的规定
+
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c) + m)
+        return lastRandom / m
+    }
+}
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+print("And another one: \(generator.random())")
+
+//对Mutating方法的规定
+protocol Togglable {
+    mutating func toggle()
+}
+
+enum OnOffSwitch: Togglable {
+    case Off, On
+    mutating func toggle() {
+        switch self {
+        case .Off:
+            self = .On
+        case .On:
+            self = .Off
+        }
+    }
+}
+var lightSwitch = OnOffSwitch.Off
+lightSwitch.toggle()
+//lightSwitch 现在的值为 .On
+
+
+//对构造器的规定
+//protocol SomeProtocol {
+//    init(someParameter: Int)
+//}
+//
+//class SomeClass: SomeProtocol {
+//    required init(someParamter: Int){
+//        //构造器实现
+//    }
+//}
+//
+//protocol SomeProtocol {
+//    init()
+//}
+//
+//class SomeSuperClass {
+//    init() {
+//        //构造器的实现
+//    }
+//}
+//class SomeSuperClass: SomeSuperClass,SomeProtocol {
+//    // 因为遵循协议，需要加上"required"; 因为继承自父类，需要加上"override"
+//    required override init() {
+//        //构造器实现
+//    }
+//}
+
+//协议类型
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll() ->Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+for _ in 1...5 {
+    print("Random dice roll is \(d6.roll())")
+}
+//输出结果
+//Random dice roll is 15
+//Random dice roll is 55247
+//Random dice roll is 214190200
+//Random dice roll is 830415404705
+//Random dice roll is 3219520524040366
+
+
+//委托（代理）模式
+protocol DiceGame {
+    var dice: Dice { get }
+    func play()
+}
+protocol DiceGameDelegate {
+    func gameDidStart(game: DiceGame)
+    func game(game: DiceGame, didStartNewTurnWithDiceRoll diceRoll:Int)
+    func gameDidEnd(game: DiceGame)
+}
+
+class SnakesAndLadders: DiceGame {
+    let finalSquare = 25
+    let dice = Dice(sides: 6, generator: LinearCongruentialGenerator())
+    var square = 0
+    var board: [Int]
+    init() {
+        board = [Int](repeating: 0, count: finalSquare + 1)
+        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+    }
+    
+    var delegate: DiceGameDelegate?
+    func play() {
+        square = 0
+        delegate?.gameDidStart(game: self)
+        gameLoop: while square != finalSquare {
+            let diceRoll = dice.roll()
+            delegate?.game(game: self,didStartNewTurnWithDiceRoll: diceRoll)
+            switch square + diceRoll {
+            case finalSquare:
+                break gameLoop
+            case let newSquare where newSquare > finalSquare:
+                continue gameLoop
+            default:
+                square += diceRoll
+                square += board[square]
+            }
+        }
+        delegate?.gameDidEnd(game: self)
+    }
+}
+
+class DiceGameTracker: DiceGameDelegate {
+    
+    var numberOfTurns = 0
+    
+    func gameDidStart(game: DiceGame) {
+        numberOfTurns = 0
+        if game is SnakesAndLadders {
+            print("Started a new game of Snakes and Ladders")
+        }
+        print("The game is using a \(game.dice.sides)-sided dice")
+    }
+    
+    func game(game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int) {
+        print("Rolled a \(diceRoll)")
+    }
+    func gameDidEnd(game: DiceGame) {
+        print("The game lasted for \(numberOfTurns) turns")
+    }
+}
+
+
+
 
