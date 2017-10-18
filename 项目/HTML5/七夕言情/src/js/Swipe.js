2e9645a5e61a03a935013b558b3eb678a7419589
qijinliang <15914115336@163.com>
2017-10-18 15:27:37 +0800

    布局的自适应动态调整

--- 项目/HTML5/七夕言情/src/js/Swipe.js
+++ 项目/HTML5/七夕言情/src/js/Swipe.js
@@ -8,37 +8,35 @@
  * @param {[type]} options   [参数]
  */
 function Swipe(container) {
-    //获取第一个子节点
+    // 获取第一个子节点
     var element = container.find(":first");
-
-    //滑动对象
     var swipe = {};
 
-    //li页面数量
+    // li页面数量
     var slides = element.find("li");
 
-    //获取容器尺寸
+    // 获取容器尺寸
     var width = container.width();
     var height = container.height();
 
-    //设置li页面总宽度
+    // 设置li页面总宽度
     element.css({
         width: (slides.length * width) + 'px',
         height: height + 'px'
     });
 
-    //设置每一个页面li的宽度
+    // 设置每一个页面li的宽度
     $.each(slides, function(index) {
-        var slide = slides.eq(index); //获取到每一个li元素
+        var slide = slides.eq(index); // 获取到每一个li元素
         slide.css({
             width: width + 'px',
             height: height + 'px'
         });
     });
 
-    //监控完成与移动
+    // 监控完成与移动
     swipe.scrollTo = function(x, speed) {
-        //执行动画移动
+        // 执行动画移动
         element.css({
             'transition-timing-function' : 'linear',
             'transition-duration'        : speed + 'ms',
