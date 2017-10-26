79fae12942a98d414a452393f541abd6aaae281d
金亮齐 <15914115336@163.com>
2017-10-26 21:06:18 +0800

    添加内容

--- 语法练习/Swift学习教程(下)/Swift学习第一篇.playground/Contents.swift
+++ 语法练习/Swift学习教程(下)/Swift学习第一篇.playground/Contents.swift
@@ -2,4 +2,43 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//我的第一个Swift程序
+var myString = "Hello,World!"
+print(myString)
+
+//编码规范推荐使用方法
+let a = 1 + 2
+let b = 3+4
+
+//整型字面量
+42
+//浮点型字面量
+3.14159
+//字符串型字面量
+"Hello, World"
+//布尔型字面量
+true
+
+//类型别名
+typealias Feet = Int
+var distance: Feet = 100
+print(distance)
+
+//类型判断
+
+//varA 会被推测为Int类型
+var varA = 42
+print(varA)
+
+//varB 会被推测为Double类型
+var varB = 3.14159
+print(varB)
+
+//varV 会被推测为Double类型
+var varC = 3 + 0.14159
+print(varC)
+
+
+//变量的命名
+
+
