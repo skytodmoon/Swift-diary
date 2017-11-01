423c6818093b89f562cc00d2cc7cbee5be1e03ec
qijinliang <15914115336@163.com>
2017-11-01 09:19:29 +0800

    壁纸

--- 语法练习/Swift学习教程(下)/Swift学习第二篇.playground/Contents.swift
+++ 语法练习/Swift学习教程(下)/Swift学习第二篇.playground/Contents.swift
@@ -1,3 +1,12 @@
 //: Playground - noun: a place where people can play
 
+//Swift 字符串
+// 使用字符串字面量
+var stringA = "Hello, World!"
+print( stringA )
+
+// String 实例化
+var stringB = String("Hello, World!")
+print( stringB )
+
 
