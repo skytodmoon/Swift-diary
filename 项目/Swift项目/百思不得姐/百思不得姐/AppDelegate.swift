db8bca4dc6a2dfaaf0319ea1d3782d66314f27a1
qijinliang <15914115336@163.com>
2017-11-03 14:49:22 +0800

    添加图片素材

--- 项目/Swift项目/百思不得姐/百思不得姐/AppDelegate.swift
+++ 项目/Swift项目/百思不得姐/百思不得姐/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        let rootViewController = MainViewController()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible();
         return true
     }
 
