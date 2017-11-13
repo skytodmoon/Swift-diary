05ab1c2dd04ce4fa376cedabc9d318ec256a124b
qijinliang <15914115336@163.com>
2017-11-13 14:36:41 +0800

    推荐数据
    推荐数据

--- 项目/Swift项目/百思不得姐/百思不得姐/Classes/Main(主页)/Controller/MainNavigationController.swift
+++ 项目/Swift项目/百思不得姐/百思不得姐/Classes/Main(主页)/Controller/MainNavigationController.swift
@@ -21,15 +21,32 @@ class MainNavigationController: UINavigationController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        
+        if childViewControllers.count > 0 {
+            let button = UIButton(type: .custom)
+            button.setTitle("返回", for: .normal)
+            button.setImage(UIImage(named:"navigationButtonReturn"), for: .normal)
+            button.setImage(UIImage(named:"navigationButtonReturnClick"), for: .highlighted)
+            button.addTarget(self, action: #selector(back), for: .touchUpInside)
+            button.setTitleColor(UIColor.darkGray, for: .normal)
+            button.setTitleColor(UIColor.red, for: .highlighted)
+            button.sizeToFit()
+            button.contentEdgeInsets = UIEdgeInsetsMake(0, -10, 0, 0)
+            button.contentHorizontalAlignment = .left
+            viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: button)
+            
+            // 隐藏tabbar
+            viewController.hidesBottomBarWhenPushed = true
+            
+        }
+        super.pushViewController(viewController, animated: animated)
+        
     }
-    */
+}
 
+extension MainNavigationController {
+    @objc func back() {
+        popViewController(animated: true)
+    }
 }
