27ed136b61accdfd8a6b6739295117ad376e2320
qijinliang <15914115336@163.com>
2017-11-14 16:08:08 +0800

    添加老外视频教程
    添加老外视频教程

---
+++ 项目/Swift项目/FacebookFeed/FacebookFeed/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FacebookFeed
+//
+//  Created by 金亮齐 on 2017/11/14.
+//  Copyright © 2017年 金亮齐. All rights reserved.
+//
+
+import UIKit
+
+class FeedController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1.0)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
---
+++ 项目/Swift项目/FacebookFeed/FacebookFeedTests/FacebookFeedTests.swift
@@ -0,0 +1,36 @@
+//
+//  FacebookFeedTests.swift
+//  FacebookFeedTests
+//
+//  Created by 金亮齐 on 2017/11/14.
+//  Copyright © 2017年 金亮齐. All rights reserved.
+//
+
+import XCTest
+@testable import FacebookFeed
+
+class FacebookFeedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
