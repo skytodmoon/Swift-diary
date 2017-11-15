3a30eaffea7e292cb6645a131927472c7a2d8989
金亮齐 <15914115336@163.com>
2017-11-15 22:06:54 +0800

    添加新的内容
    添加新的内容

--- 项目/Swift项目/Youtube/Youtube/AppDelegate.swift
+++ 项目/Swift项目/Youtube/Youtube/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.makeKeyAndVisible()
+        
+        let layout = UICollectionViewFlowLayout()
+        window?.rootViewController = UINavigationController(rootViewController: HomeViewController(collectionViewLayout: layout))
         return true
     }
 
