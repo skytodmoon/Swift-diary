6128aac61954e175e3a7f68f9e05db911215c727
金亮齐 <15914115336@163.com>
2017-11-15 21:49:56 +0800

    回滚
    回滚

--- 项目/Swift项目/FacebookFeed/FacebookFeed/Classes/Model/Post.swift
+++ 项目/Swift项目/FacebookFeed/FacebookFeed/Classes/Model/Post.swift
@@ -8,44 +8,21 @@
 
 import UIKit
 
-<<<<<<< HEAD
-class Post:SafeJsonObject{
-=======
-class Post{
->>>>>>> parent of 6f466b2a... 解析json
+class Post:NSObject{
     
     var name: String?
-    var statusText: String?
     var profileImageName: String?
+    var statusText: String?
     var statusImageName: String?
-    var numLikes: Int?
-    var numContent: Int?
+    var numLikes: NSNumber?
+    var numComments: NSNumber?
     var statusInageUrl: String?
-<<<<<<< HEAD
     
     var location: Location?
-        
-    override func setValue(value: AnyObject?, forKey key: String) {
-        if key == "location"{
-            location = Location()
-            location?.setValuesForKeysWithDictionary(value as! [String: AnyObject])
-        }else{
-            super.setValue(value, forKey: key)
-        }
-    }
 }
 
-class SafeJsonObject: NSObject {
-    
-    override func setValue(value: AnyObject?, forKey key: String) {
-        let selectorString = "set\(key.uppercaseString.characters.first)\(String(key.characters.dropFirst())):"
-        let selector = Selector(selectorString)
-        if respondsToSelector(selector){
-            super.setValue(value, forKey: key)
-        }
-    }
-=======
->>>>>>> parent of 6f466b2a... 解析json
+class Location: NSObject {
+    var city: String?
+    var state: String?
 }
-
 
