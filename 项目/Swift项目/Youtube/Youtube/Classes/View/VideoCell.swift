4fc70326d067051dc39850709bc5b55c9b92dd6b
qijinliang <15914115336@163.com>
2017-11-16 11:38:05 +0800

    处理布局
    处理布局

--- 项目/Swift项目/Youtube/Youtube/Classes/View/VideoCell.swift
+++ 项目/Swift项目/Youtube/Youtube/Classes/View/VideoCell.swift
@@ -15,11 +15,75 @@ class VideoCell: UICollectionViewCell {
         setupViews()
     }
     
+    let thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "taylor_swift_blank_space")
+        imageView.contentMode = .ScaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let userProfileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "taylor_swift_profile")
+        imageView.layer.cornerRadius = 22
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.blackColor()
+        return view
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Taylor Swift - Blank Space"
+        return label
+    }()
+    
+    let subtitleTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.text = "TaylorSwiftVEVO *  1,604,684,607 views * 2 years ago"
+        textView.textContainerInset = UIEdgeInsetsMake(0, -4, 0, 0)
+        textView.textColor = UIColor.lightGrayColor()
+        return textView
+    }()
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     func setupViews(){
-        backgroundColor = UIColor.blueColor()
+
+        addSubview(thumbnailImageView)
+        addSubview(userProfileImageView)
+        addSubview(separatorView)
+        addSubview(titleLabel)
+        addSubview(subtitleTextView)
+        
+        addConstraintsWithFormat("H:|-16-[v0]-16-|", views: thumbnailImageView)
+        addConstraintsWithFormat("H:|-16-[v0(44)]", views: userProfileImageView)
+        addConstraintsWithFormat("V:|-16-[v0]-8-[v1(44)]-16-[v2(1)]|", views: thumbnailImageView,userProfileImageView,separatorView)
+        addConstraintsWithFormat("H:|[v0]|", views: separatorView)
+        
+        addConstraint(NSLayoutConstraint(item: titleLabel,attribute: .Top,relatedBy: .Equal,toItem: thumbnailImageView,attribute: .Bottom, multiplier: 1,constant: 8))
+        
+        addConstraint(NSLayoutConstraint(item: titleLabel,attribute: .Left,relatedBy: .Equal,toItem: userProfileImageView,attribute: .Right, multiplier: 1,constant: 8))
+        
+        addConstraint(NSLayoutConstraint(item: titleLabel,attribute: .Right,relatedBy: .Equal,toItem: thumbnailImageView,attribute: .Right, multiplier: 1,constant: 0))
+        
+        addConstraint(NSLayoutConstraint(item: titleLabel,attribute: .Height,relatedBy: .Equal,toItem: self,attribute: .Height, multiplier: 0,constant: 20))
+        
+        addConstraint(NSLayoutConstraint(item: subtitleTextView,attribute: .Top,relatedBy: .Equal,toItem: titleLabel,attribute: .Bottom, multiplier: 1,constant: 4))
+        
+        addConstraint(NSLayoutConstraint(item: subtitleTextView,attribute: .Left,relatedBy: .Equal,toItem: userProfileImageView,attribute: .Right, multiplier: 1,constant: 8))
+        
+        addConstraint(NSLayoutConstraint(item: subtitleTextView,attribute: .Right,relatedBy: .Equal,toItem: thumbnailImageView,attribute: .Right, multiplier: 1,constant: 0))
+        
+        addConstraint(NSLayoutConstraint(item: subtitleTextView,attribute: .Height,relatedBy: .Equal,toItem: self,attribute: .Height, multiplier: 0,constant: 30))
     }
 }
