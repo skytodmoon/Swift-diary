e54134ce6474de8360aad5f9b9c73968b8eaf1d9
qijinliang <15914115336@163.com>
2017-11-21 14:14:27 +0800

    完成布局
    完成布局

--- 项目/FbMessenger/FbMessenger/ViewController.swift
+++ 项目/FbMessenger/FbMessenger/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendsController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView!.backgroundColor = UIColor.whiteColor()
+        collectionView?.registerClass(FriendCell.self, forCellWithReuseIdentifier: cellId)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
     }
-
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath)
+        
+        return cell
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(view.frame.width, 100)
+    }
 
 }
 
+
+class FriendCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews(){
+        backgroundColor = UIColor.blueColor()
+    }
+}
