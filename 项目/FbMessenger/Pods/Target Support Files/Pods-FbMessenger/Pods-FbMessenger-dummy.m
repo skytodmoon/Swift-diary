1a0661665dfc4db7e1cee37f6954ac9d8c0dd6b1
qijinliang <15914115336@163.com>
2017-11-21 14:01:36 +0800

    创建新例子项目
    创建新例子项目

---
+++ 项目/FbMessenger/Pods/Target Support Files/Pods-FbMessenger/Pods-FbMessenger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FbMessenger : NSObject
+@end
+@implementation PodsDummy_Pods_FbMessenger
+@end
---
+++ 项目/FbMessenger/Pods/Target Support Files/Pods-FbMessengerTests/Pods-FbMessengerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FbMessengerTests : NSObject
+@end
+@implementation PodsDummy_Pods_FbMessengerTests
+@end
