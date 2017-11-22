23721a3f522fdc55c5b035c8d395c89579c58cc9
qijinliang <15914115336@163.com>
2017-11-22 15:05:22 +0800

    完成空间布局
    完成空间布局

--- 项目/Swift项目/FbMessenger/FbMessenger/Classes/Controller/ViewController.swift
+++ 项目/Swift项目/FbMessenger/FbMessenger/Classes/Controller/ViewController.swift
@@ -52,6 +52,36 @@ class FriendCell: BaseCell {
         return view
     }()
     
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Mark Zuckeroery"
+        label.font = UIFont.systemFontOfSize(18)
+        return label
+    }()
+    
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Your frieds message and something else..."
+        label.textColor = UIColor.darkGrayColor()
+        label.font = UIFont.systemFontOfSize(14)
+        return label
+    }()
+
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "12:05 pm"
+        label.font = UIFont.systemFontOfSize(16)
+        label.textAlignment = .Right
+        return label
+    }()
+    
+    let hasReadImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .ScaleAspectFill
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
 
     override func setupViews(){
         
@@ -61,6 +91,7 @@ class FriendCell: BaseCell {
         setupContainerView()
         
         profileImageView.image = UIImage(named: "headImage")
+        hasReadImageView.image = UIImage(named: "headImage")
         
         addConstraintsWithFormat("H:|-12-[v0(68)]", views: profileImageView)
         addConstraintsWithFormat("V:[v0(68)]", views: profileImageView)
@@ -73,11 +104,23 @@ class FriendCell: BaseCell {
     
     private func setupContainerView(){
         let containerView = UIView()
-        containerView.backgroundColor = UIColor.redColor()
         addSubview(containerView)
         
         addConstraintsWithFormat("H:|-90-[v0]|", views: containerView)
-        addConstraintsWithFormat("V:[v0(60)]", views: containerView)
+        addConstraintsWithFormat("V:[v0(50)]", views: containerView)
+        
+        addConstraint(NSLayoutConstraint(item: containerView,attribute: .CenterY,relatedBy: .Equal,toItem: self,attribute: .CenterY,multiplier: 1,constant: 0))
+    
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(messageLabel)
+        containerView.addSubview(timeLabel)
+        containerView.addSubview(hasReadImageView)
+        
+        containerView.addConstraintsWithFormat("H:|[v0][v1(80)]-12-|", views: nameLabel,timeLabel)
+        containerView.addConstraintsWithFormat("V:|[v0][v1(24)]|", views: nameLabel,messageLabel)
+        containerView.addConstraintsWithFormat("H:|[v0]-8-[v1(20)]-12-|", views: messageLabel,hasReadImageView)
+        containerView.addConstraintsWithFormat("V:|[v0(24)]", views: timeLabel)
+        containerView.addConstraintsWithFormat("V:[v0(20)]|", views: hasReadImageView)
     }
 }
 
