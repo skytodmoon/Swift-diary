29c4985bf5c4ba6b8bb227a5324f829f502b8e29
qijinliang <15914115336@163.com>
2017-12-12 09:08:32 +0800

    移除
    移除

--- 项目/Swift项目/有妖气漫画/有妖气漫画/AppDelegate.swift
+++ 项目/Swift项目/有妖气漫画/有妖气漫画/AppDelegate.swift
@@ -7,18 +7,37 @@
 //
 
 import UIKit
+import Alamofire
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    lazy var reachability: NetworkReachabilityManager? = {
+        
+        return NetworkReachabilityManager(host: "http://app.u17.com")
+    }()
+    
+    var orientaion: UIInterfaceOrientationMask = .portrait
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        configBase()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = MainTabbarController()
+        window?.makeKeyAndVisible()
         return true
     }
-
+    func configBase(){
+        
+        IQKeyboardManager.sharedManager().enable = true
+        IQKeyboardManager.sharedManager().shouldResignOnTouchOutside = true
+        
+    }
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -42,5 +61,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension UIApplication{
+    
 }
 
---
+++ 项目/Swift项目/有妖气漫画/有妖气漫画/Classes/Other/Global.swift
@@ -0,0 +1,17 @@
+//
+//  Global.swift
+//  有妖气漫画
+//
+//  Created by 金亮齐 on 2017/12/11.
+//  Copyright © 2017年 金亮齐. All rights reserved.
+//
+import UIKit
+import Kingfisher
+import MJRefresh
+import SnapKit
+
+
+extension String {
+    static let searchHistoryKey = "searchHistoryKey"
+    static let sexTypeKey = "sexTypeKey"
+}
